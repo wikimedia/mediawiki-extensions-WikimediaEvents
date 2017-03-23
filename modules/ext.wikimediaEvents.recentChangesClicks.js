836a90b54ebfa2611d915aea033e216a532bf2f1
Roan Kattouw <roan.kattouw@gmail.com>
2017-03-22 18:58:02 -0700

    RCFilters: Log click actions on RC page and on pages linked from there
    This logs clicks on the diff, history, page, user page, user talk,
    user contribs, user block and rollback links on Special:Recentchanges,
    and also clicks on those links (as well as edit, undo, thank and patrol)
    on pages reached by clicking a link on the RC page.
    
    The latter is done by adding ?fromrc=1 to the URL of the link
    as it is clicked.
    
    Bug: T158458
    Change-Id: I1a79088f344908758e00c97599cdeebf4ad1789a

---
+++ modules/ext.wikimediaEvents.recentChangesClicks.js
@@ -0,0 +1,100 @@
+/*!
+ * Track clicks on the Recentchanges page
+ *
+ * @see https://meta.wikimedia.org/wiki/Schema:ChangesListClickTracking
+ * @author Roan Kattouw <rkattouw@wikimedia.org>
+ */
+( function ( $, mw ) {
+	var linkTypes = {
+		'.mw-changeslist-diff': 'diff',
+		'.mw-changeslist-history': 'history',
+		'.mw-changeslist-title': 'page',
+		'.mw-userlink': 'user',
+		'.mw-usertoollinks-talk': 'talk',
+		'.mw-usertoollinks-contribs': 'contribs',
+		'.mw-usertoollinks-block': 'block',
+		'.mw-rollback-link a': 'rollback',
+		'.mw-diff-edit a': 'edit',
+		'.mw-diff-undo a': 'undo',
+		'.mw-thanks-thank-link': 'thank',
+		'.patrollink a': 'patrol'
+	};
+
+	$( function () {
+		var uri = new mw.Uri();
+
+		function trackClick( type, fromPage ) {
+			mw.track( 'event.ChangesListClickTracking', {
+				linkType: type,
+				enhancedFiltersEnabled: !!mw.user.options.get( 'rcenhancedfilters' ),
+				userId: mw.user.getId(),
+				sessionId: mw.user.sessionId(),
+				fromPage: fromPage,
+				fromQuery: uri.getQueryString()
+			} );
+		}
+
+		function getPageType() {
+			if ( uri.query.action === 'history' ) {
+				return 'history';
+			} else if ( uri.query.diff !== undefined ) {
+				return 'diff';
+			}
+			return 'page';
+		}
+
+		if ( $( 'body' ).hasClass( 'mw-special-Recentchanges' ) ) {
+			$( '.mw-changeslist' ).on( 'click', 'a[href]', function ( e ) {
+				var selector,
+					type = 'unknown',
+					$link = $( this ),
+					target = new mw.Uri( $link.attr( 'href' ) );
+				if ( e.which === 3 ) {
+					return;
+				}
+
+				// Add fromrc=1 to the URL
+				target.extend( { fromrc: 1 } );
+				$link.attr( 'href', target.toString() );
+
+				// Figure out the link type
+				for ( selector in linkTypes ) {
+					if ( $link.is( selector ) ) {
+						type = linkTypes[ selector ];
+						break;
+					}
+				}
+
+				// Update current URL, the dynamic RC code could have changed it
+				uri = new mw.Uri( location.href );
+
+				// Log an event
+				trackClick( type, 'Recentchanges' );
+			} );
+		} else if ( uri.query.fromrc === '1' ) {
+			$( 'body' ).on( 'click', 'a[href]', function ( e ) {
+				var selector, type,
+					$link = $( this );
+
+				if ( e.which === 3 ) {
+					return;
+				}
+
+				// Figure out the link type, and only log links of known types
+				for ( selector in linkTypes ) {
+					if ( $link.is( selector ) ) {
+						type = linkTypes[ selector ];
+						break;
+					}
+				}
+				if ( type === undefined ) {
+					return;
+				}
+
+				// Log an event
+				trackClick( type, getPageType() );
+			} );
+		}
+
+	} );
+}( jQuery, mediaWiki ) );
