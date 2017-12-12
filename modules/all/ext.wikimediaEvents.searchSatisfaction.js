4d6d5e905de945647fc24795d172c787bbb33128
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-12-12 14:26:21 -0800

    Turn on second mlr test for hewiki
    Re-uses the same sampling rates as last time around on hewiki. Depends
    on I681e1e724 being deployed first to setup the appropriate backend
    triggers.
    
    Bug: T182616
    Change-Id: I7bc0ddd86da966a8f49b27a4ddf9aa93074a6b39

--- modules/all/ext.wikimediaEvents.searchSatisfaction.js
+++ modules/all/ext.wikimediaEvents.searchSatisfaction.js
@@ -114,7 +114,9 @@
 		function initialize( session ) {
 
 			var sessionId = session.get( 'sessionId' ),
-				validBuckets = [],
+				validBuckets = mw.config.get( 'wgDBname' ) === 'hewiki' ?
+					[ 'control', 'ltr-1024', 'ltr-1024-i' ] :
+					[],
 				sampleSize = ( function () {
 					var dbName = mw.config.get( 'wgDBname' ),
 						// Provides a place to handle wiki-specific sampling,
@@ -135,9 +137,6 @@
 								test: 350,
 								subTest: null
 							},
-							// .0005 works out to ~2.7k sessions per week.
-							// .15 increases that to 810k per week. Giving
-							// 160k sessions per bucket per week.
 							enwiki: {
 								test: 2000,
 								subTest: null
@@ -189,6 +188,10 @@
 							zhwiki: {
 								test: 100,
 								subTest: null
+							},
+							hewiki: {
+								test: 0.8112,
+								subTest: 0.8767
 							}
 						};
 					if ( subTests[ dbName ] ) {
