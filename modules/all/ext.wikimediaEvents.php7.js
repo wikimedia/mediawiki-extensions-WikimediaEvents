f1eb547df2a7cb666a2d0ac4d04ed5f9e1c7bbb6
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-02-26 10:14:00 +0100

    Use a cookie to persist the seed for php7's a/b test
    Bug: T216676
    Change-Id: I844163d038ffefba7a8fcb8f6db1a4f09e82ac22

--- modules/all/ext.wikimediaEvents.php7.js
+++ modules/all/ext.wikimediaEvents.php7.js
@@ -12,11 +12,30 @@
 		return;
 	}
 
+	/**
+	 * Get a random seed. Generates one if not already present in a cookie.
+	 *
+	 * @return {string}
+	 */
+	function getSeed() {
+		// We persist this ID across browsing sessions in
+		// order to progressively move users to PHP7, rather than re-extract
+		// anons for every browsing session.
+		var id = $.cookie( 'mwPhp7Seed' );
+		if ( !id || isNaN( parseInt( id, 16 ) ) ) {
+			// We don't need sampling beyond 1 in 1000, so let's save
+			// some bytes over the wire by truncating to a 3-digit hex.
+			id = mw.user.generateRandomSessionId().slice( 0, 3 );
+			$.cookie( 'mwPhp7Seed', id, { expires: 60, path: '/' } );
+		}
+		return id;
+	}
+
 	function ensureCookie() {
 		// Ensure the PHP_ENGINE cookie is at the desired version.
 		var sampleRate = mw.config.get( 'wgWMEPhp7SamplingRate', 0 ),
 			hasCookie = /PHP_ENGINE=php7/.test( document.cookie ),
-			inSample = mw.eventLog.sessionInSample( sampleRate );
+			inSample = mw.eventLog.randomTokenMatch( sampleRate, getSeed() );
 
 		if ( inSample && !hasCookie ) {
 			$.cookie( 'PHP_ENGINE', 'php7', { expires: 7, path: '/' } );
