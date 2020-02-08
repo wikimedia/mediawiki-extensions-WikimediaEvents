5e926becb5813387a922e61b77fba07b7836d73e
Timo Tijhof <krinklemail@gmail.com>
2020-02-08 22:15:21 +0000

    Move owners data to OWNERS.md add structure test to enforce
    This removes the 'visualEditorFeatureUse' key as required for
    the tests to pass, because it was added for enabling a schema,
    not for JS files, and we don't need to track enabled schemas
    here anymore given they have little to no perf impact after
    the 2019 refactorings in EventLogging.
    
    Change-Id: Ic4f90894eec3c8ed1108f67e8a6bd5c296f6537a

---
+++ OWNERS.md
@@ -0,0 +1,81 @@
+# Code ownership for components of WikimediaEvents
+
+The code in this repository is loaded globally for all users of Wikipedia and other public
+Wikimedia Foundation wikis. Including for all page types, namespaces, skins, and devices.
+
+Shipping code here comes at a cost, and it is important that campaigns leave a trace
+to contact owners. This way, if optimisations are proposed, there is a way to contact
+for code review, and to routinely evaluate whether gathered data is still actively being
+used and providing value so as to free up budget for other teams to deploy campaigns.
+
+## mw-js-deprecate
+
+* Since: March 2014
+* Files: deprecate.js
+* Contact: https://www.mediawiki.org/wiki/Wikimedia_Performance_Team
+
+Powers <https://grafana.wikimedia.org/dashboard/db/mw-js-deprecate>.
+
+## StatsD from JavaScript
+
+* Since: Dec 2014.
+* Files: statsd.js
+* Contact: Analytics team
+
+Handles the mw-track topics for `counter.*`, `timing.*`, and `gauge.*`.
+Documentation at <https://wikitech.wikimedia.org/wiki/Graphite#statsv>.
+
+## Search satisfaction
+
+* Since: October 2015
+* Files: searchSatisfaction.js
+* Contact: https://www.mediawiki.org/wiki/Wikimedia_Search_Platform
+
+## WMF-specific event logging
+
+* Since: June 2016
+* Files: events.js
+* Contact: Product Infrastructure team
+
+Proxies the mw-track topic `wikimedia.event.*` to `event.*`.
+Background at <https://phabricator.wikimedia.org/T138659>.
+
+## Reading depth
+
+* Since: February 2017
+* Files: readingDepth.js
+* Contact: https://www.mediawiki.org/wiki/Readers/Web/Team
+
+Details at <https://phabricator.wikimedia.org/T155639>.
+
+## Citation usage
+
+* Since: June 2018
+* Files: citationUsage.js
+* Contact: Research
+
+Details at <https://phabricator.wikimedia.org/T191086>
+
+## Wikibase
+
+* Since: July 2018
+* Files: completionClicks.js
+* Contact: Search Platform
+
+Details at <https://phabricator.wikimedia.org/T196186>.
+
+## Click-tracking for MobileFrontend
+
+* Since: July 2019
+* Files: mobileWebUIActions.js
+* Contact: Readers Web"
+
+Details at <https://phabricator.wikimedia.org/T220016>.
+
+## Inuka page views
+
+* Since: February 2020
+* Files: InukaPageView.js
+* Contact: Inuka team
+
+Details at <https://phabricator.wikimedia.org/T238029>.
---
+++ tests/phpunit/OwnersStructureTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace WikimediaEvents\Tests;
+
+use LogicException;
+
+/**
+ * @coversNothing
+ */
+class OwnersStructureTest extends \PHPUnit\Framework\TestCase {
+	/** @var array */
+	private static $ownerSections;
+
+	public static function setUpBeforeClass() : void {
+		// Parse the owners data and store it in an array.
+		$sections = [];
+		$lines = file( __DIR__ . '/../../OWNERS.md',  FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES );
+		$section = null;
+		foreach ( $lines as $line ) {
+			if ( strpos( $line, '* ' ) === 0 ) {
+				list( $label, $value ) = explode( ':', substr( $line, 2 ), 2 );
+				if ( $label === 'Files' ) {
+					$section[$label] = array_map( 'trim', explode( ',', $value ) );
+				} else {
+					$section[$label] = trim( $value );
+				}
+			}
+			if ( strpos( $line, '## ' ) === 0 ) {
+				if ( $section !== null ) {
+					// Commit previous section
+					$sections[ $section['title'] ] = $section;
+				}
+				$section = [ 'title' => trim( substr( $line, 2 ) ) ];
+			}
+		}
+		if ( $section !== null ) {
+			// Commit last section
+			$sections[ $section['title'] ] = $section;
+			$section = null;
+		}
+		self::$ownerSections = $sections;
+	}
+
+	private function getResources() : array {
+		$extension = json_decode(
+			file_get_contents( __DIR__ . '/../../extension.json' ),
+			JSON_OBJECT_AS_ARRAY
+		);
+		$modules = $extension['ResourceModules'];
+		$resources = [];
+		foreach ( $modules as $moduleName => $moduleInfo ) {
+			foreach ( $moduleInfo as $key => $value ) {
+				$files = [];
+				switch ( $key ) {
+					case 'scripts':
+						$files = $value;
+						break;
+					case 'skinScripts':
+						foreach ( $value as $media => $styles ) {
+							$files = array_merge( $files, $styles );
+						}
+						break;
+					case 'dependencies':
+					case 'targets':
+						// ignore
+						break;
+					default:
+						throw new LogicException( "Unknown module info key: {$key}" );
+				}
+				foreach ( $files as $file ) {
+					$resources[ basename( $file ) ] = $moduleName;
+				}
+			}
+		}
+		return $resources;
+	}
+
+	public function testOwnersFile() {
+		$expected = [ 'Files', 'Contact', 'Since' ];
+		foreach ( self::$ownerSections as $title => $section ) {
+			$actual = array_intersect( $expected, array_keys( $section ) );
+			$this->assertSame( $expected, $actual, "Labels under OWNERS.md § $title" );
+		}
+	}
+
+	/**
+	 * @depends testOwnersFile
+	 */
+	public function testResourceOwnersFile() {
+		$ownedFiles = [];
+		foreach ( self::$ownerSections as $section ) {
+			foreach ( $section['Files'] as $file ) {
+				$ownedFiles[] = $file;
+			}
+		}
+
+		$resources = $this->getResources();
+		foreach ( $resources as $file => $moduleName ) {
+			$this->assertTrue(
+				in_array( $file, $ownedFiles ),
+				"File $file in $moduleName has an owner"
+			);
+		}
+		foreach ( $ownedFiles as $file ) {
+			$this->assertTrue(
+				isset( $resources[$file] ),
+				"Owned file $file is a registered resource"
+			);
+		}
+	}
+}
