30731c2c748a0ea34f798628fe7961b928572a23
Timo Tijhof <krinklemail@gmail.com>
2020-04-11 19:20:05 +0100

    PrefUpdate: Add property tracking filter
    * Bump major version from '1' to '2'.
    
    * Fix `@const` which is afaik not a supported annotation.
      For docs about non-function members, Doxygen expects `@var`.
    
    * Remove the title-based filter. It's not clear what this was
      for as it was effectively already covering all known ways to
      set preferences (if there were other ways, would we not want
      to include them?) and especially with the property whitelist
      there is also no longer a volume concern if there was any.
    
      This makes the code easier to reason about and less error
      prone. In particular, use of global RequestContext is
      unexpected here and not supported by core (if it was, it'd
      be passed in the hook).
    
    * Add test coverage.
    
    Bug: T249894
    Change-Id: Ia4a745dd9fb930c41432a92751afba1918ba5527

--- includes/PrefUpdateInstrumentation.php
+++ includes/PrefUpdateInstrumentation.php
@@ -4,8 +4,7 @@ namespace WikimediaEvents;
 
 use EventLogging;
 use FormatJson;
-use RequestContext;
-use Title;
+use MWTimestamp;
 use User;
 
 /**
@@ -20,19 +19,87 @@ use User;
 class PrefUpdateInstrumentation {
 
 	/**
-	 * @const int REV_ID The revision ID of the PrefUpdate schema that we're using.
+	 * @var int The revision ID of the PrefUpdate schema that we're using.
 	 */
 	private const REV_ID = 19799589;
 
 	/**
-	 * Logs a <a href="https://meta.wikimedia.org/wiki/Schema:PrefUpdate">PrefUpdate</a> event for
-	 * every preference that the user has changed.
+	 * @var string Bumped when the nature of the data collected in the log is changed.
+	 */
+	private const MAJOR_VERSION = '2';
+
+	/**
+	 * The maximum length of a property value tracked with VALUE_WELLKNOWN_SHORT.
 	 *
-	 * Note well that logging only occurs when the user changes their preferences via the
-	 * Preferences or MobileOptions special pages, the latter of which is provided by the
-	 * MobileFrontend extension, or via the
-	 * <a href="https://www.mediawiki.org/wiki/API:Options">options API</a>.
+	 * This is currently fairly liberal at 50 chars. Realistically anything even
+	 * close to that is unlikely to be a non-user-generated value from a
+	 * software-predefined choice. If you find values are being cropped, consider
+	 * adding a dedicated aggregation type for them so that data analysts have
+	 * an easier time working with the data, and to make graph-plotting easier
+	 * as well.
 	 *
+	 * @var int
+	 */
+	private const SHORT_MAX_LEN = 50;
+
+	/**
+	 * Indicates that a property does only holds one of several well-known
+	 * and predefined choices. By themselves these may be seen in public,
+	 * and are not user-generated. Note that in relation to a user this is
+	 * still considered personal information. For use in PROPERTY_TRACKLIST.
+	 *
+	 * @var int
+	 */
+	private const VALUE_WELLKNOWN_SHORT = 1;
+
+	/**
+	 * Indicates that a property holds potentially personal information formatted
+	 * as a newline-separated list. The instrumentation will report the value
+	 * as a count (zero or more). For use in PROPERTY_TRACKLIST.
+	 *
+	 * @var int
+	 */
+	private const VALUE_NEWLINE_COUNT = 2;
+
+	/**
+	 * @var string[] List of preferences (aka user properties, aka user options)
+	 * to track via EventLogging when they are changed (T249894)
+	 */
+	private const PROPERTY_TRACKLIST = [
+		// Reading Web team
+		'skin' => self::VALUE_WELLKNOWN_SHORT,
+		'mfMode' => self::VALUE_WELLKNOWN_SHORT,
+		'mf_amc_optin' => self::VALUE_WELLKNOWN_SHORT,
+		'VectorSkinVersion' => self::VALUE_WELLKNOWN_SHORT,
+		'popups' => self::VALUE_WELLKNOWN_SHORT,
+		'popupsreferencepreviews' => self::VALUE_WELLKNOWN_SHORT,
+
+		// Editing team
+		'discussiontools-betaenable' => self::VALUE_WELLKNOWN_SHORT,
+		'betafeatures-auto-enroll'  => self::VALUE_WELLKNOWN_SHORT,
+
+		// AHT
+		'echo-notifications-blacklist' => self::VALUE_NEWLINE_COUNT,
+		'email-blacklist' => self::VALUE_NEWLINE_COUNT,
+
+		// Growth team
+		'growthexperiments-help-panel-tog-help-panel' => self::VALUE_WELLKNOWN_SHORT,
+		'growthexperiments-homepage-enable' => self::VALUE_WELLKNOWN_SHORT,
+		'growthexperiments-homepage-pt-link' => self::VALUE_WELLKNOWN_SHORT,
+	];
+
+	/**
+	 * Log an event when a tracked user preference is changed.
+	 *
+	 * Note that logging must be explicitly enabled for a property in order for
+	 * tracking to take place.
+	 *
+	 * This hook is triggered when User::saveOptions() is called after User::setOption().
+	 * For example when submitting from Special:Preferences or Special:MobileOptions,
+	 * or from any client-side interfaces that use api.php?action=options (ApiOptions)
+	 * to store user preferences.
+	 *
+	 * @see https://meta.wikimedia.org/wiki/Schema:PrefUpdate
 	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/UserSaveOptions
 	 *
 	 * @param User $user The user whose options are being saved
@@ -44,39 +111,62 @@ class PrefUpdateInstrumentation {
 		array &$options,
 		array $originalOptions
 	) : void {
-		// Modified version of original method from the Echo extension
-		$out = RequestContext::getMain()->getOutput();
-		// Capture user options saved via Special:Preferences, Special:MobileOptions or ApiOptions
+		$now = MWTimestamp::now( TS_MW );
 
-		// TODO (mattflaschen, 2013-06-13): Ideally this would be done more cleanly without
-		// looking explicitly at page names and URL parameters.
-		// Maybe a userInitiated flag passed to saveSettings would work.
-		if ( self::isKnownSettingsPage( $out->getTitle() )
-			|| ( defined( 'MW_API' ) && $out->getRequest()->getVal( 'action' ) === 'options' )
-		) {
-			$commonData = [
-				'version' => '1',
-				'userId' => $user->getId(),
-				'saveTimestamp' => wfTimestampNow(),
-			];
-
-			foreach ( $options as $optName => $optValue ) {
-				// loose comparision is required since some of the values
-				// are not consistent in the two variables, eg, '' vs false
-				$originalOptionValue = $originalOptions[$optName] ?? null;
-				if ( $originalOptionValue != $optValue ) {
-					$event = [
-						'property' => $optName,
-						// Encode value as JSON.
-						// This is parseable and allows a consistent type for validation.
-						'value' => FormatJson::encode( $optValue ),
-						'isDefault' => User::getDefaultOption( $optName ) == $optValue,
-						'bucketedUserEditCount' => self::getBucketedUserEditCount( $user ),
-					] + $commonData;
+		foreach ( $options as $optName => $optValue ) {
+			$prevValue = $originalOptions[$optName] ?? null;
+			// Use loose comparision because the implicit default form declared in PHP
+			// often uses integers and booleans, whereas the stored format often uses
+			// strings (e.g. "" vs false)
+			if ( $prevValue != $optValue ) {
+				$event = self::createPrefUpdateEvent( $user, $optName, $optValue, $now );
+				if ( $event !== false ) {
 					EventLogging::logEvent( 'PrefUpdate', self::REV_ID, $event );
 				}
 			}
 		}
+	}
+
+	/**
+	 * Format a changed user preference as a PrefUpdate event, or false to send none.
+	 *
+	 * @param User $user
+	 * @param string $optName
+	 * @param string $optValue
+	 * @param string $now
+	 * @return false|array
+	 */
+	private static function createPrefUpdateEvent( User $user, $optName, $optValue, $now ) {
+		$trackType = self::PROPERTY_TRACKLIST[$optName] ?? null;
+		if ( $trackType === null ) {
+			// Not meant to be tracked.
+			return false;
+		}
+
+		if ( $trackType === self::VALUE_WELLKNOWN_SHORT ) {
+			if ( strlen( $optValue ) > self::SHORT_MAX_LEN ) {
+					trigger_error( "Unexpected value for $optName in PrefUpdate", E_USER_ERROR );
+					return false;
+			}
+			$trackedValue = $optValue;
+		} elseif ( $trackType === self::VALUE_NEWLINE_COUNT ) {
+			$trackedValue = count( preg_split( '/\n/', $optValue, -1, PREG_SPLIT_NO_EMPTY ) );
+		} else {
+			trigger_error( "Unknown handler for $optName in PrefUpdate", E_USER_ERROR );
+			return false;
+		}
+
+		return [
+			'version' => self::MAJOR_VERSION,
+			'userId' => $user->getId(),
+			'saveTimestamp' => $now,
+			'property' => $optName,
+			// Encode value as JSON.
+			// This is parseable and allows a consistent type for validation.
+			'value' => FormatJson::encode( $trackedValue ),
+			'isDefault' => User::getDefaultOption( $optName ) == $optValue,
+			'bucketedUserEditCount' => self::getBucketedUserEditCount( $user ),
+		];
 	}
 
 	/**
@@ -108,25 +198,5 @@ class PrefUpdateInstrumentation {
 		}
 
 		return '0 edits';
-	}
-
-	/**
-	 * Gets whether or not the page is a settings page, i.e. either the Preferences or
-	 * MobileOptions special page, the latter of which is provided by the MobileFrontend extension.
-	 *
-	 * @param Title|null $title The title that represents the page
-	 * @return bool
-	 */
-	private static function isKnownSettingsPage( Title $title = null ) : bool {
-		$allowedPages = [ 'Preferences', 'MobileOptions' ];
-		if ( $title === null ) {
-			return false;
-		}
-		foreach ( $allowedPages as $page ) {
-			if ( $title->isSpecial( $page ) ) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
---
+++ tests/phpunit/PrefUpdateInstrumentationTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace WikimediaEvents\Tests;
+
+use User;
+use Wikimedia\TestingAccessWrapper;
+use WikimediaEvents\PrefUpdateInstrumentation;
+
+/**
+ * @covers \WikimediaEvents\PrefUpdateInstrumentation
+ */
+class PrefUpdateInstrumentationTest extends \MediaWikiIntegrationTestCase {
+
+	private const MOCK_TIME = '20110401080000';
+
+	public function setUp() : void {
+		parent::setUp();
+		$this->setMwGlobals( 'wgDefaultSkin', 'fallback' );
+	}
+
+	public function providePrefUpdate() {
+		$user = $this->createMock( User::class );
+		$user->method( 'getId' )->willReturn( 4 );
+		$user->method( 'getEditCount' )->willReturn( 42 );
+
+		yield 'Enable foo' => [ $user, 'foo', '1', false ];
+
+		yield 'Set Minerva skin' => [ $user, 'skin', 'minerva', [
+			'version' => '2',
+			'userId' => 4,
+			'saveTimestamp' => '20110401080000',
+			'property' => 'skin',
+			'value' => '"minerva"',
+			'isDefault' => false,
+			'bucketedUserEditCount' => '5-99 edits',
+		] ];
+
+		yield 'Set default skin' => [ $user, 'skin', 'fallback', [
+			'version' => '2',
+			'userId' => 4,
+			'saveTimestamp' => '20110401080000',
+			'property' => 'skin',
+			'value' => '"fallback"',
+			'isDefault' => true,
+			'bucketedUserEditCount' => '5-99 edits',
+		] ];
+
+		yield 'Store bogus skin value' => [ $user, 'skin', str_repeat( 'x', 100 ), false,
+			'Unexpected value for skin'
+		];
+
+		yield 'Set new VectorSkinVersion' => [ $user, 'VectorSkinVersion', '2', [
+			'version' => '2',
+			'userId' => 4,
+			'saveTimestamp' => '20110401080000',
+			'property' => 'VectorSkinVersion',
+			'value' => '"2"',
+			'isDefault' => false,
+			'bucketedUserEditCount' => '5-99 edits',
+		] ];
+
+		yield 'Add to email-blacklist' => [ $user, 'email-blacklist', "31\n4\n159", [
+			'version' => '2',
+			'userId' => 4,
+			'saveTimestamp' => '20110401080000',
+			'property' => 'email-blacklist',
+			'value' => '3',
+			'isDefault' => false,
+			'bucketedUserEditCount' => '5-99 edits',
+		] ];
+
+		yield 'Clear email-blacklist' => [ $user, 'email-blacklist', "", [
+			'version' => '2',
+			'userId' => 4,
+			'saveTimestamp' => '20110401080000',
+			'property' => 'email-blacklist',
+			'value' => '0',
+			'isDefault' => true,
+			'bucketedUserEditCount' => '5-99 edits',
+		] ];
+	}
+
+	/**
+	 * @dataProvider providePrefUpdate
+	 * @param string $name
+	 * @param string $value
+	 * @param false|array $expect
+	 * @param null|string $error
+	 */
+	public function testCreatePrefUpdateEvent( $user, $name, $value, $expect, $error = null ) {
+		$prefUpdate = TestingAccessWrapper::newFromClass( PrefUpdateInstrumentation::class );
+
+		if ( $error ) {
+			$this->expectError();
+			$this->expectErrorMessage( $error );
+		}
+		$this->assertSame(
+			$expect,
+			$prefUpdate->createPrefUpdateEvent( $user, $name, $value, self::MOCK_TIME )
+		);
+	}
+
+}
