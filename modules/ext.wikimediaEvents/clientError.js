d3b7a00f55188f49cc3a2e4072e9e7127e48c1ec
Jdlrobson <jrobson@wikimedia.org>
2020-10-15 17:07:06 +0000

    Revert "clientError: Adds 'is_logged_in' tag to aid filtering"
    This reverts commit 6cc1a92aa17e86f400df54cb1fa6bb80ffc0460c.
    
    Reason for revert: Incompatible with the schema.
    
    Bug: T256173
    Change-Id: If8e6abd5edbb05ef4f58267b7fda5fdae3e43450

--- modules/ext.wikimediaEvents/clientError.js
+++ modules/ext.wikimediaEvents/clientError.js
@@ -82,12 +82,9 @@
 				url: location.href,
 				// Normalized stack trace string
 				// eslint-disable-next-line camelcase
-				stack_trace: obj.stackTrace,
+				stack_trace: obj.stackTrace
 				// Tags that can be specified as-needed
-				tags: {
-					// eslint-disable-next-line camelcase
-					is_logged_in: !mw.user.isAnon()
-				}
+				// tags: {}
 			} ) );
 		} );
 	}
