ff7f55f570d4953ca387e72d65b98ddbe168f9cc
Jan Drewniak <jan.drewniak@gmail.com>
2021-02-19 13:56:20 +0100

    Add userEditBucket to searchSatisfaction
    Adds the userEditBucket property to the searchSatisfaction schema
    and bumps the version of the searchSatisfaction schema to 1.4.0
    
    Bug: T272991
    Depends-On: I4074b08411a12cc19751679b48c4274c5621338c
    Change-Id: I8822d2aa104f1fa2952061e34b5bfea82df49fea

--- modules/ext.wikimediaEvents/searchSatisfaction.js
+++ modules/ext.wikimediaEvents/searchSatisfaction.js
@@ -452,6 +452,7 @@
 
 			evt.skin = skin;
 			evt.isAnon = mw.user.isAnon();
+			evt.userEditBucket = mw.wikimediaEvents.getEditCountBucket( mw.config.get( 'wgUserEditCount' ) );
 
 			// Is the user using the Vector skin? If so, then include which version of the skin
 			// they're using and which version of the search widget they're seeing.
