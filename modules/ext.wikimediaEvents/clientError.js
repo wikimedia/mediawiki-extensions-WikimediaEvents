wikimedia/mediawiki-extensions-WikimediaEvents

commit 44148d523787000dbfbe1ccf9eae52ce887db022

Revise file url logging rules
Define shouldLogFileUrl code more readable.

Apply following changes:
* Do not log file uris prefixed with "C:\\", "javascript:", "jar:"
- these are generally
added by bookmarklets. We don't want
to log errors associated for the same reason as blob:
* Do not log URIs with "/inj_js/" in the path. This is common on our
deployed wikis and cannot be filtered since it is not a prefix or
suffix
* We were dropping various errors which have no file url
in Chrome due to loading code from localStorage. We now
are in a good state to log these.

Bug: T266517
Change-Id: I53b03dd5d56ad18ae9d12d7d60a068aeb41f2b73

modules/ext.wikimediaEvents/clientError.js
@@ -160,6 +160,42 @@
 	}
 
 	/**
+	 * Check whether error logging is supported for the current file URI
+	 *
+	 * @param {string} [fileUrl]
+	 * @return {bool}
+	 */
+	function shouldLogFileUrl( fileUrl ) {
+		// file url may not be defined given cached scripts run from localStorage.
+		// If not explicitly set to undefined (T266517) to support filtering but still log.
+		fileUrl = fileUrl || 'undefined';
+		//
+		// If the two URLs differ only by a fragment identifier (e.g.
+		// 'example.org' vs. 'example.org#Section'), we consider them
+		// to be matching.
+		// Per spec, obj.url should never contain a fragment identifier,
+		// yet we have observed this in the wild in several instances,
+		// hence we must strip the identifier from both.
+		//
+		return fileUrl.split( '#' )[ 0 ] === location.href.split( '#' )[ 0 ] ||
+			// Various errors originate from scripts we do not control. These may be
+			// prefixed by "blob:" or "javascript:" or one of the browser extensions.
+			// These are not logged but may in future be diverted
+			// to another channel (see T259383 for more information).
+			// eslint-disable-next-line no-script-url
+			fileUrl.indexOf( 'javascript:' ) === 0 ||
+			// Common pattern seen in the wild. Short for "inject JS".
+			fileUrl.indexOf( '/inj_js/' ) > -1 ||
+			fileUrl.indexOf( 'blob:' ) === 0 ||
+			fileUrl.indexOf( 'jar:' ) === 0 ||
+			// from Windows file system.
+			fileUrl.indexOf( 'C:\\' ) === 0 ||
+			fileUrl.indexOf( 'chrome-extension://' ) === 0 ||
+			fileUrl.indexOf( 'safari-extension://' ) === 0 ||
+			fileUrl.indexOf( 'moz-extension://' ) === 0;
+	}
+
+	/**
 	 * Parses out an error descriptor from the error's stack trace.
 	 *
 	 * @param {Mixed} error
@@ -240,8 +276,6 @@
 	 * @return {boolean}
 	 */
 	function shouldLog( descriptor ) {
-		var fileUrl;
-
 		if ( !descriptor ) {
 			return false;
 		}
@@ -261,14 +295,7 @@
 			return false;
 		}
 
-		fileUrl = descriptor.fileUrl;
-		if ( !fileUrl ||
-			fileUrl.split( '#' )[ 0 ] === location.href.split( '#' )[ 0 ] ||
-			fileUrl.indexOf( 'blob:' ) === 0 ||
-			fileUrl.indexOf( 'chrome-extension://' ) === 0 ||
-			fileUrl.indexOf( 'safari-extension://' ) === 0 ||
-			fileUrl.indexOf( 'moz-extension://' ) === 0
-		) {
+		if ( shouldLogFileUrl( descriptor.fileUrl ) ) {
 			// When the error lacks a URL, or the URL is defaulted to page
 			// location, the stack trace is rarely meaningful, if ever.
 			//

commit cbf4563894c79d2a4aed7e030037330ea6fada0a

clientError: Restore client error logging output
The function returns true if the error is not subject to
error logging, introduced in 44148d523

* The file_url should be logged as undefined if missing
  and sent to the intake service
* The message key should forward errorMessage per line 236.

Additional:
Function name shouldn't be confusing.

The function returns true if the error is not subject to
error logging.

Bug: T277094
Change-Id: I7093ed4b170c5b42ad25f3e6da9ea68f30992dc0

modules/ext.wikimediaEvents/clientError.js
@@ -162,13 +162,10 @@
 	/**
 	 * Check whether error logging is supported for the current file URI
 	 *
-	 * @param {string} [fileUrl]
+	 * @param {string} fileUrl
 	 * @return {boolean}
 	 */
-	function shouldLogFileUrl( fileUrl ) {
-		// file url may not be defined given cached scripts run from localStorage.
-		// If not explicitly set to undefined (T266517) to support filtering but still log.
-		fileUrl = fileUrl || 'undefined';
+	function shouldIgnoreFileUrl( fileUrl ) {
 		//
 		// If the two URLs differ only by a fragment identifier (e.g.
 		// 'example.org' vs. 'example.org#Section'), we consider them
@@ -274,7 +271,9 @@
 		return {
 			errorClass: ( errorObject && errorObject.constructor.name ) || '',
 			errorMessage: normalizeErrorMessage( errorLoggerObject.errorMessage ),
-			fileUrl: errorLoggerObject.url,
+			// file url may not be defined given cached scripts run from localStorage.
+			// If not explicitly set to undefined (T266517) to support filtering but still log.
+			fileUrl: errorLoggerObject.url || 'undefined',
 			stackTrace: stackTrace,
 			errorObject: errorObject
 		};
@@ -306,7 +305,7 @@
 			return false;
 		}
 
-		if ( shouldLogFileUrl( descriptor.fileUrl ) ) {
+		if ( shouldIgnoreFileUrl( descriptor.fileUrl ) ) {
 			// When the error lacks a URL, or the URL is defaulted to page
 			// location, the stack trace is rarely meaningful, if ever.
 			//
@@ -367,7 +366,7 @@
 			// eslint-disable-next-line camelcase
 			error_class: descriptor.errorClass,
 			// Message included with the Error object
-			message: descriptor.message,
+			message: descriptor.errorMessage,
 			// URL of the file causing the error
 			// eslint-disable-next-line camelcase
 			file_url: descriptor.fileUrl,