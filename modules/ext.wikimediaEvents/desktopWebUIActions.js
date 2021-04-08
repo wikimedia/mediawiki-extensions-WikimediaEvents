29771001912e04e813053672f6d758fc871ef0b6
Sam Smith <phuedx@wikimedia.org>
2021-04-08 08:35:30 +0100

    desktopWebUIActionsTracking: Tidy redundant variables
    ... and bolster inline documentation.
    
    Change-Id: Ia6b4f4223f4a14502203ba4ec042c298b57e37b0

--- modules/ext.wikimediaEvents/desktopWebUIActions.js
+++ modules/ext.wikimediaEvents/desktopWebUIActions.js
@@ -4,10 +4,7 @@
  * Launch task: https://phabricator.wikimedia.org/T250282
  * Schema: https://meta.wikimedia.org/wiki/Schema:DesktopWebUIActionsTracking
  */
-var eventLog = mw.eventLog,
-	getEditCountBucket = mw.wikimediaEvents.getEditCountBucket,
-	sampleSize = require( './config.json' ).desktopWebUIActionsTracking || 0,
-	pop = sampleSize ? 1 / sampleSize : 0,
+var sampleSize = require( './config.json' ).desktopWebUIActionsTracking || 0,
 	skinVersion;
 
 /**
@@ -30,23 +27,27 @@ function logEvent( action, name ) {
 			// Ideally this would use an mw.config value but this will do for now
 			skinVersion: skinVersion,
 			skin: mw.config.get( 'skin' ),
-			editCountBucket: getEditCountBucket( mw.config.get( 'wgUserEditCount' ) ),
+			editCountBucket: mw.wikimediaEvents.getEditCountBucket( mw.config.get( 'wgUserEditCount' ) ),
 			isSidebarCollapsed: checkbox ? !checkbox.checked : false,
 			token: mw.user.sessionId()
 		};
 		if ( name ) {
 			data.name = name;
 		}
-		eventLog.logEvent( 'DesktopWebUIActionsTracking', data );
+		mw.eventLog.logEvent( 'DesktopWebUIActionsTracking', data );
 	}
 }
 
-// Don't initialize the instrument if the user isn't using the Vector skin or if their pageview
-// isn't in the sample.
+// Don't initialize the instrument if:
 //
-// The schema works on skins other than Vector however for now it's limited to Vector (see
-// WikimediaEventsHooks::getModuleFile) to aid the work of data analysts.
-if ( !eventLog.eventInSample( pop ) ) {
+// * The user isn't using the Vector skin - this module is only delivered when the user is using the
+//   Vector skin (see WikimediaEvents\WikimediaEventsHooks::getModuleFile)
+// * $wgWMEDesktopWebUIActionsTracking is falsy
+// * The pageview isn't in the sample
+//
+// Note well the schema works on skins other than Vector but for now it's limited to it to aid the
+// work of data analysts.
+if ( !sampleSize || !mw.eventLog.eventInSample( 1 / sampleSize ) ) {
 	return;
 }
 
