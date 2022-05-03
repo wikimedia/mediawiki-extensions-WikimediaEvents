22847a08abc7ef170bd8530233562d4aaf3017af
Jon Robson <jdlrobson@gmail.com>
2022-05-03 13:59:08 -0700

    Test for undefined offset error
    Bug: T307019
    Change-Id: I1dd8ccca61d86ffc53fa7ac85ea4902ff1dc9853

--- tests/phpunit/unit/WebABTest/WebABTestArticleIdFactoryTest.php
+++ tests/phpunit/unit/WebABTest/WebABTestArticleIdFactoryTest.php
@@ -2,6 +2,10 @@
 
 namespace WikimediaEvents\Tests;
 
+use FauxRequest;
+use RequestContext;
+use Title;
+use WikimediaEvents\PageSplitter\PageSplitterInstrumentation;
 use WikimediaEvents\WebABTest\WebABTestArticleIdFactory;
 
 /**
@@ -25,5 +29,60 @@ class WebABTestArticleIdFactoryTest extends \MediaWikiUnitTestCase {
 		);
 
 		$this->assertEquals( [ 'control', 'treatment' ], $buckets );
+	}
+
+	public function provideT307019() {
+		return [
+			[ 1, 'treatment' ],
+			[ 1012765, 'control' ],
+			[ 720999, 'treatment' ],
+			[ 1575109, 'treatment' ],
+			[ 332219, 'control' ],
+			[ 496036, 'treatment' ],
+			[ 2031011, 'treatment' ],
+			[ 4693, 'control' ],
+			[ 528228, 'treatment' ],
+			[ 341646, 'treatment' ]
+		];
+	}
+
+	/**
+	 * @dataProvider provideT307019
+	 */
+	public function testT307019( $articleID, $expectedBucket ) {
+		// A/B/C split test with 50% sampling.
+		$subject = new PageSplitterInstrumentation( 0.5, [ 'a', 'b', 'c' ] );
+		$experimentConfig = [
+			'name' => 'skin-vector-toc-experiment',
+			'enabled' => true,
+			'buckets' => [
+					'unsampled' => [
+							'samplingRate' => 0
+					],
+					'control' => [
+							'samplingRate' => 0.5
+					],
+					'treatment' => [
+							'samplingRate' => 0.5
+					],
+			]
+		];
+		$context = $this->createMock( RequestContext::class );
+		$context->method( 'getRequest' )->willReturn( new FauxRequest() );
+		$title = $this->createMock( Title::class );
+		$title->method( 'getArticleID' )->willReturn( $articleID );
+		$context->method( 'getTitle' )->willReturn( $title );
+
+		$webABTestArticleIdFactory = new WebABTestArticleIdFactory();
+		$bucketKeys = array_keys( $experimentConfig['buckets'] );
+
+		$ab = $webABTestArticleIdFactory->makeWebABTestArticleIdStrategy(
+			$webABTestArticleIdFactory->filterExcludedBucket( $bucketKeys ),
+			1 - $experimentConfig['buckets']['unsampled']['samplingRate'],
+			'tableofcontents',
+			$context
+		);
+		$bucket = $ab->getBucket();
+		$this->assertSame( $bucket, $expectedBucket );
 	}
 }
