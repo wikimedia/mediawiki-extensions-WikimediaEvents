d00620892fb6f0e3a79cf8989a7a930026ce10a8
Timo Tijhof <krinkle@fastmail.com>
2022-08-12 21:27:24 +0100

    phpEngine: Add sticky option
    Follows-up ede160c767 (Ifee0d51cc50). Expand some of the docs.
    
    Bug: T311388
    Change-Id: I3fb55cfc432999b2cb19cf9341417bd05054e541

--- modules/ext.wikimediaEvents/phpEngine.js
+++ modules/ext.wikimediaEvents/phpEngine.js
@@ -1,25 +1,37 @@
 /*!
- * Pick/remove users from using different php interpreters
- * by managing the PHP_ENGINE cookie.
- * @see https://phabricator.wikimedia.org/T216676 for the original introduction
+ * Sample clients in or out of a PHP_ENGINE cookie.
+ *
+ * Changelog:
+ * - 2019 original, https://phabricator.wikimedia.org/T216676.
+ * - 2022 revision, https://phabricator.wikimedia.org/T311388.
  */
 
 function ensureCookie() {
-	// Ensure the PHP_ENGINE cookie is at the desired version.
-	var moduleConfig = require( './config.json' ),
-		version = moduleConfig.newPHPVersion;
-	// If no version is defined in the configuration,
-	// we don't want to process the rest of the function
+	var moduleConfig = require( './config.json' );
+	var version = moduleConfig.newPHPVersion;
 	if ( !version ) {
+		// No-op if a "new" PHP version is not yet (or no longer) defined.
+		// Optimization: Avoid cookie I/O for removal here,
+		// they should be unused now and can naturally expire.
 		return;
 	}
 
-	var hasCookie = $.cookie( 'PHP_ENGINE' ) !== null,
-		inSample = mw.eventLog.sessionInSample( moduleConfig.newPHPSamplingRate );
+	var hasCookie = $.cookie( 'PHP_ENGINE' ) !== null;
+	var inSample = mw.eventLog.sessionInSample( moduleConfig.newPHPSamplingRate );
 
+	// Ensure the PHP_ENGINE cookie is at the desired version.
+	// In order for reductions in sampling rate to take immediate effect,
+	// this also removes existing cookies.
+	//
+	// Developers may set PHP_ENGINE_STICKY=1 in order to opt-in.
+	// Usage:
+	//
+	//     $.cookie( 'PHP_ENGINE', version, { expires: 30, path: '/' } );
+	//     $.cookie( 'PHP_ENGINE_STICKY', '1', { expires: 30, path: '/' } );
+	//
 	if ( inSample && !hasCookie ) {
 		$.cookie( 'PHP_ENGINE', version, { expires: 7, path: '/' } );
-	} else if ( !inSample && hasCookie ) {
+	} else if ( !inSample && hasCookie && !$.cookie( 'PHP_ENGINE_STICKY' ) ) {
 		$.removeCookie( 'PHP_ENGINE', { path: '/' } );
 	}
 }
