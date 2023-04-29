53590568b8a197b018bd340977f66ea9394d8d00
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2023-04-29 11:02:16 +0530

    Add Probenet library and fetch code
    Created a new networkprobe module which gets loaded and initiates a probe when a specific cookie is set.
    
    Bug: T334417
    Change-Id: I143c1fdcf563403789018aae02595ea2640d620c

---
+++ modules/ext.wikimediaEvents.networkprobe/recipe.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+
+const RECIPE = {
+	name: 'Latency Test',
+	type: 'http_get',
+	pulses: 3,
+	pulse_delay: 500,
+	pulse_timeout: 15000,
+	url_metadata: true,
+	serial_probe: false,
+	targets: [
+		{
+			name: 'esams',
+			target: 'https://measure-esams.wikimedia.org/measure'
+		},
+		{
+			name: 'eqiad',
+			target: 'https://measure-eqiad.wikimedia.org/measure'
+		},
+		{
+			name: 'drmrs',
+			target: 'https://measure-drmrs.wikimedia.org/measure'
+		},
+		{
+			name: 'codfw',
+			target: 'https://measure-codfw.wikimedia.org/measure'
+		},
+		{
+			name: 'eqsin',
+			target: 'https://measure-eqsin.wikimedia.org/measure'
+		},
+		{
+			name: 'ulsfo',
+			target: 'https://measure-ulsfo.wikimedia.org/measure'
+		}
+	],
+	ctx: {
+		server: 'WikimediaEvents-static-1'
+	}
+};
+
+module.exports = RECIPE;
