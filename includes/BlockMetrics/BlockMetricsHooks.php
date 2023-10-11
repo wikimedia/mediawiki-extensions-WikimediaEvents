dd5b3041093f864d4cf5f5bf568b7b3a53d8bd28
Tim Starling <tstarling@wikimedia.org>
2023-10-11 13:24:49 +1100

    Stop using deprecated method User::isBlockedFromCreateAccount()
    Also use an early return to reduce the indenting level of
    onPermissionErrorAudit.
    
    Depends-On: Icd75025665282a34d8854bda267a0eb3d759719c
    Bug: T345683
    Change-Id: I7d19eda46b24650ec5534499d34936b754cd075a

--- includes/BlockMetrics/BlockMetricsHooks.php
+++ includes/BlockMetrics/BlockMetricsHooks.php
@@ -7,6 +7,7 @@ use MediaWiki\Extension\EventBus\EventFactory;
 use MediaWiki\Extension\EventLogging\EventLogging;
 use MediaWiki\Linker\LinkTarget;
 use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Permissions\Hook\PermissionErrorAuditHook;
 use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\User\UserFactory;
@@ -48,89 +49,91 @@ class BlockMetricsHooks implements PermissionErrorAuditHook {
 		array $errors
 	): void {
 		// Ignore RIGOR_QUICK checks for performance; those won't check blocks anyway.
-		if ( $action === 'createaccount' && $rigor !== PermissionManager::RIGOR_QUICK ) {
-			// Possible block error keys from Block\BlockErrorFormatter::getBlockErrorMessageKey()
-			$blockedErrorKeys = [
-				'blockedtext',
-				'autoblockedtext',
-				'blockedtext-partial',
-				'systemblockedtext',
-				'blockedtext-composite'
-			];
-			// Possible block error keys from GlobalBlocking extension GlobalBlocking::getUserBlockDetails()
-			$globalBlockedErrorKeys = [
-				'globalblocking-ipblocked',
-				'globalblocking-ipblocked-range',
-				'globalblocking-ipblocked-xff',
-				// WikimediaMessages versions
-				'wikimedia-globalblocking-ipblocked',
-				'wikimedia-globalblocking-ipblocked-range',
-				'wikimedia-globalblocking-ipblocked-xff',
-			];
-			$isApi = defined( 'MW_API' ) || defined( 'MW_REST_API' );
+		if ( $action !== 'createaccount' || $rigor === PermissionManager::RIGOR_QUICK ) {
+			return;
+		}
+		// Possible block error keys from Block\BlockErrorFormatter::getBlockErrorMessageKey()
+		$blockedErrorKeys = [
+			'blockedtext',
+			'autoblockedtext',
+			'blockedtext-partial',
+			'systemblockedtext',
+			'blockedtext-composite'
+		];
+		// Possible block error keys from GlobalBlocking extension GlobalBlocking::getUserBlockDetails()
+		$globalBlockedErrorKeys = [
+			'globalblocking-ipblocked',
+			'globalblocking-ipblocked-range',
+			'globalblocking-ipblocked-xff',
+			// WikimediaMessages versions
+			'wikimedia-globalblocking-ipblocked',
+			'wikimedia-globalblocking-ipblocked-range',
+			'wikimedia-globalblocking-ipblocked-xff',
+		];
+		$isApi = defined( 'MW_API' ) || defined( 'MW_REST_API' );
 
-			$blockedErrorMsgs = $globalBlockedErrorMsgs = [];
-			foreach ( $errors as $error ) {
-				$errorMsg = Message::newFromSpecifier( $error );
-				$errorKey = $errorMsg->getKey();
-				if ( in_array( $errorKey, $blockedErrorKeys, true ) ) {
-					$blockedErrorMsgs[] = $errorMsg;
-				} elseif ( in_array( $errorKey, $globalBlockedErrorKeys, true ) ) {
-					$globalBlockedErrorMsgs[] = $errorMsg;
-				}
+		$blockedErrorMsgs = $globalBlockedErrorMsgs = [];
+		foreach ( $errors as $error ) {
+			$errorMsg = Message::newFromSpecifier( $error );
+			$errorKey = $errorMsg->getKey();
+			if ( in_array( $errorKey, $blockedErrorKeys, true ) ) {
+				$blockedErrorMsgs[] = $errorMsg;
+			} elseif ( in_array( $errorKey, $globalBlockedErrorKeys, true ) ) {
+				$globalBlockedErrorMsgs[] = $errorMsg;
 			}
-			$allErrorMsgs = array_merge( $blockedErrorMsgs, $globalBlockedErrorMsgs );
+		}
+		$allErrorMsgs = array_merge( $blockedErrorMsgs, $globalBlockedErrorMsgs );
 
-			if ( !$allErrorMsgs ) {
-				return;
+		if ( !$allErrorMsgs ) {
+			return;
+		}
+
+		$user = $this->userFactory->newFromUserIdentity( $user );
+
+		$block = null;
+		// Prefer the local block over the global one if both are set. This is somewhat arbitrary.
+		if ( $blockedErrorMsgs ) {
+			$block = MediaWikiServices::getInstance()->getBlockManager()
+				->getCreateAccountBlock( $user, RequestContext::getMain()->getRequest(), true );
+		} elseif ( $globalBlockedErrorMsgs ) {
+			$block = $user->getGlobalBlock();
+		}
+
+		if ( $block ) {
+			$context = RequestContext::getMain();
+			foreach ( $allErrorMsgs as $msg ) {
+				$msg->setContext( $context )->useDatabase( false )->inLanguage( 'en' );
 			}
-
-			$user = $this->userFactory->newFromUserIdentity( $user );
-
-			$block = null;
-			// Prefer the local block over the global one if both are set. This is somewhat arbitrary.
-			if ( $blockedErrorMsgs ) {
-				$block = $user->isBlockedFromCreateAccount();
-			} elseif ( $globalBlockedErrorMsgs ) {
-				$block = $user->getGlobalBlock();
-			}
-
-			if ( $block ) {
-				$context = RequestContext::getMain();
-				foreach ( $allErrorMsgs as $msg ) {
-					$msg->setContext( $context )->useDatabase( false )->inLanguage( 'en' );
-				}
-				$rawExpiry = $block->getExpiry();
-				if ( wfIsInfinity( $rawExpiry ) ) {
-					$expiry = 'infinity';
-				} else {
-					$expiry = wfTimestamp( TS_ISO_8601, $rawExpiry );
-				}
-				$event = [
-					'$schema' => self::SCHEMA,
-					'block_id' => json_encode( $block->getIdentifier() ),
-					// @phan-suppress-next-line PhanTypeMismatchDimFetchNullable
-					'block_type' => Block::BLOCK_TYPES[ $block->getType() ] ?? 'other',
-					'block_expiry' => $expiry,
-					'block_scope' => $blockedErrorMsgs ? 'local' : 'global',
-					'error_message_keys' => array_map( static function ( Message $msg ) {
-						return $msg->getKey();
-					}, $allErrorMsgs ),
-					'error_messages' => array_map( static function ( Message $msg ) {
-						return $msg->plain();
-					}, $allErrorMsgs ),
-					'user_ip' => $user->getRequest()->getIP(),
-					'is_api' => $isApi,
-				];
-				$event += $this->eventFactory->createMediaWikiCommonAttrs( $user );
-				$this->submitEvent( 'mediawiki.accountcreation_block', $event );
+			$rawExpiry = $block->getExpiry();
+			if ( wfIsInfinity( $rawExpiry ) ) {
+				$expiry = 'infinity';
 			} else {
-				LoggerFactory::getInstance( 'WikimediaEvents' )->warning( 'Could not find block', [
-					'errorKeys' => implode( ',', array_map( static function ( Message $msg ) {
-						return $msg->getKey();
-					}, $allErrorMsgs ) ),
-				] );
+				$expiry = wfTimestamp( TS_ISO_8601, $rawExpiry );
 			}
+			$event = [
+				'$schema' => self::SCHEMA,
+				'block_id' => json_encode( $block->getIdentifier() ),
+				// @phan-suppress-next-line PhanTypeMismatchDimFetchNullable
+				'block_type' => Block::BLOCK_TYPES[ $block->getType() ] ?? 'other',
+				'block_expiry' => $expiry,
+				'block_scope' => $blockedErrorMsgs ? 'local' : 'global',
+				'error_message_keys' => array_map( static function ( Message $msg ) {
+					return $msg->getKey();
+				}, $allErrorMsgs ),
+				'error_messages' => array_map( static function ( Message $msg ) {
+					return $msg->plain();
+				}, $allErrorMsgs ),
+				'user_ip' => $user->getRequest()->getIP(),
+				'is_api' => $isApi,
+			];
+			$event += $this->eventFactory->createMediaWikiCommonAttrs( $user );
+			$this->submitEvent( 'mediawiki.accountcreation_block', $event );
+		} else {
+			LoggerFactory::getInstance( 'WikimediaEvents' )->warning( 'Could not find block', [
+				'errorKeys' => implode( ',', array_map( static function ( Message $msg ) {
+					return $msg->getKey();
+				}, $allErrorMsgs ) ),
+			] );
 		}
 	}
 
