10d6eea3a56218f4922ca95bb9c34f9cbdc5a587
Kosta Harlan <kharlan@wikimedia.org>
2024-05-23 17:21:27 +0200

    IPReputationHooks: Support disabling edit logging for older accounts
    Why:
    
    - We don't really need to analyze edits from accounts older than 90 days
      for the IP reputation research project, so we should limit the amount
      of data we collect to only what is necessary.
    
    What:
    
    - Add a configuration variable for account age threshold, and exclude
      accounts that are older in age to that threshold. The default value is
      90 days.
    
    Bug: T354597
    Change-Id: I4aa01eaa00de8f25c781d38afc5b2910cd454965

---
+++ tests/phpunit/integration/IPReputationHooksTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace WikimediaEvents\Tests\Integration;
+
+use MediaWiki\User\User;
+use MediaWikiIntegrationTestCase;
+use Wikimedia\TestingAccessWrapper;
+use Wikimedia\Timestamp\ConvertibleTimestamp;
+use WikimediaEvents\IPReputationHooks;
+
+/**
+ * @covers \WikimediaEvents\IPReputationHooks
+ */
+class IPReputationHooksTest extends MediaWikiIntegrationTestCase {
+
+	public static function provideShouldLogEditEventForUser(): array {
+		return [
+			'anonymous user' => [
+				0,
+				false,
+				true,
+			],
+			'authenticated user, no registration date' => [
+				1,
+				null,
+				false,
+			],
+			'authenticated user, older than threshold' => [
+				2,
+				'20210406112556',
+				false,
+			],
+			'authenticated user, newer than threshold' => [
+				2,
+				'20240521112556',
+				true,
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider provideShouldLogEditEventForUser
+	 */
+	public function testShouldLogEditEventForUser( $userId, $userRegistration, $expected ) {
+		$services = $this->getServiceContainer();
+		$ipReputationHooks = TestingAccessWrapper::newFromObject(
+			new IPReputationHooks(
+				$services->getMainConfig(),
+				$services->getFormatterFactory(),
+				$services->getHttpRequestFactory(),
+				$services->getMainWANObjectCache(),
+				$services->getUserFactory(),
+				$services->getUserGroupManager(),
+				$services->getService( 'EventBus.EventFactory' ),
+				$services->getService( 'EventLogging.EventSubmitter' )
+			)
+		);
+		$user = $this->createPartialMock( User::class, [
+			'getId',
+			'getRegistration'
+		] );
+		$user->method( 'getId' )->willReturn( $userId );
+		$user->method( 'getRegistration' )->willReturn( $userRegistration );
+		// Pin time to be able to deterministically check account registration age thresholds.
+		ConvertibleTimestamp::setFakeTime( '20240523120000' );
+		$this->assertEquals( $expected, $ipReputationHooks->shouldLogEditEventForUser( $user ) );
+	}
+}
