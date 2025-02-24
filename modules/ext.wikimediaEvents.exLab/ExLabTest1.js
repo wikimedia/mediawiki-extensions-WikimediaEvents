a5d504459ebfa76dbcc5587fe2b7d639e2fabe94
Clare Ming <cming@wikimedia.org>
2025-02-24 12:06:39 -0700

    Start test experiment for all enrolled users.
    Previous version only enabled CTR for those in the
    treatment group (feature === true). This removes
    the conditional to apply to all users enrolled in
    the experiment
    
    Bug: T373715
    Change-Id: I280749ec93b449a46686b9852e982475462c90e0
    (cherry picked from commit 0305d54de0db5a021f24945aab4a35ae45fb42c6)

--- modules/ext.wikimediaEvents.exLab/ExLabTest1.js
+++ modules/ext.wikimediaEvents.exLab/ExLabTest1.js
@@ -1,8 +1,6 @@
-const userExperiments = mw.config.get( 'wgMetricsPlatformUserExperiments' );
 const selector = '[data-pinnable-element-id="vector-main-menu"] .vector-pinnable-header-unpin-button';
 const friendlyName = 'pinnable-header.vector-main-menu.unpin';
 const experimentName = 'experimentation-lab-test-1-experiment';
-const featureName = 'experimentation_lab_test_1_feature';
 const STREAM_NAME = 'product_metrics.web_base';
 const SCHEMA_ID = '/analytics/product_metrics/web/base/1.3.0';
 
@@ -16,17 +14,12 @@ const SCHEMA_ID = '/analytics/product_metrics/web/base/1.3.0';
 const ExLabTest1 = {
 
 	init() {
-		if ( !mw.eventLog.isCurrentUserEnrolled( experimentName ) ||
-			!( featureName in userExperiments.assigned ) ) {
+		if ( !mw.eventLog.isCurrentUserEnrolled( experimentName ) ) {
 			return;
 		}
 		const ClickThroughRateInstrument = require( './ClickThroughRateInstrument.js' );
-
-		if ( userExperiments.assigned[ featureName ] === 'true' ) {
-			const instrument = mw.eventLog.newInstrument( STREAM_NAME, SCHEMA_ID );
-			instrument.setInstrumentName( experimentName );
-			ClickThroughRateInstrument.start( selector, friendlyName, instrument );
-		}
+		const instrument = mw.eventLog.newInstrument( STREAM_NAME, SCHEMA_ID, experimentName );
+		ClickThroughRateInstrument.start( selector, friendlyName, instrument );
 	}
 };
 
