2c8e3c72a03f22ea2eec69072d24e895057024e1
Cole White <cwhite@wikimedia.org>
2025-03-25 22:40:29 +0000

    track mw.js.deprecate metrics in Prometheus
    For mw-js-deprecate dashboard.
    
    Bug: T343020
    Change-Id: Idf34cb693508fea9649d33f81c8fe1e5245895a0

--- modules/ext.wikimediaEvents/deprecate.js
+++ modules/ext.wikimediaEvents/deprecate.js
@@ -7,10 +7,9 @@
 // Filter: Logged-in users only
 // Filter: Sample 1:100 (1%)
 if ( mw.config.get( 'wgUserName' ) && mw.eventLog.pageviewInSample( 100 ) ) {
-	mw.trackSubscribe( 'mw.deprecate', ( _, deprecated ) => {
-		mw.track(
-			'counter.mw.js.deprecate.' + ( deprecated.replace( /\W+/g, '_' ) ),
-			1
-		);
+	mw.trackSubscribe( 'mw.deprecate', ( _, feature ) => {
+		feature = feature.replace( /\W+/g, '_' );
+		mw.track( 'counter.mw.js.deprecate.' + feature, 1 );
+		mw.track( 'stats.mediawiki_deprecated_js_calls_total', 1, { feature } );
 	} );
 }
