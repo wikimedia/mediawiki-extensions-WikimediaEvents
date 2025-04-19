0469a30cc31305d3b0ee4b2f3252a40f457609d8
Timo Tijhof <krinkle@fastmail.com>
2025-04-19 02:00:59 +0100

    phpunit: Fix failing UpdatePeriodicMetricsTest
    This test started failing after Ib490666ffa4bf87234abf156bd5a landed in
    MediaWiki core because it relied on the stats *not* being flushed yet.
    
    Same issue as Ib490666ffa4bf8723, which was fixed beforehand. I missed
    that WikimediaEvents had a similar test.
    
    Change-Id: I633488a35d761b3a01dcfce6185b9481f3fc6313

--- tests/phpunit/integration/maintenance/UpdatePeriodicMetricsTest.php
+++ tests/phpunit/integration/maintenance/UpdatePeriodicMetricsTest.php
@@ -4,7 +4,6 @@ namespace WikimediaEvents\Tests\Integration\PeriodicMetrics;
 
 use InvalidArgumentException;
 use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
-use Wikimedia\Stats\Metrics\GaugeMetric;
 use Wikimedia\Stats\StatsFactory;
 use WikimediaEvents\Maintenance\UpdatePeriodicMetrics;
 use WikimediaEvents\PeriodicMetrics\IMetric;
@@ -35,7 +34,7 @@ class UpdatePeriodicMetricsTest extends MaintenanceBaseTestCase {
 			->with( 'invalidmetric' )
 			->willThrowException( new InvalidArgumentException() );
 		$this->setService( 'WikimediaEventsMetricsFactory', $mockWikimediaEventsMetricsFactory );
-		$this->setService( 'StatsFactory', $this->createNoOpMock( StatsFactory::class ) );
+		$this->setService( 'StatsFactory', StatsFactory::newNull() );
 		// Run the maintenance script
 		if ( $isInGlobalMode ) {
 			$this->maintenance->setOption( 'global-metrics', 1 );
@@ -53,7 +52,7 @@ class UpdatePeriodicMetricsTest extends MaintenanceBaseTestCase {
 
 	/** @dataProvider provideExecuteForMockIMetric */
 	public function testExecuteForMockIMetric(
-		$isVerboseMode, $metricLabels, $metricValue, $expectedOutputString
+		$isVerboseMode, $metricLabels, $metricValue, $expectedOutput, $expectedStats
 	) {
 		// Define a mock IMetric instance which will be returned by a mock WikimediaEventsMetricsFactory::newMetric
 		$mockIMetric = $this->createMock( IMetric::class );
@@ -70,34 +69,41 @@ class UpdatePeriodicMetricsTest extends MaintenanceBaseTestCase {
 			->with( 'MockIMetric' )
 			->willReturn( $mockIMetric );
 		$this->setService( 'WikimediaEventsMetricsFactory', $mockWikimediaEventsMetricsFactory );
+		$statsHelper = StatsFactory::newUnitTestingHelper();
+		$this->setService( 'StatsFactory', $statsHelper->getStatsFactory() );
 
 		// Execute the maintenance script
 		if ( $isVerboseMode ) {
 			$this->maintenance->setOption( 'verbose', 1 );
 		}
-		$this->expectOutputString( $expectedOutputString );
+		$this->expectOutputString( $expectedOutput );
 		$this->maintenance->execute();
 
-		// Check that the gauge metric with the name 'mock_metric_name' has been set to 1234.
-		$metric = $this->getServiceContainer()
-			->getStatsFactory()
-			->withComponent( 'WikimediaEvents' )
-			->getGauge( 'mock_metric_name' );
-		$samples = $metric->getSamples();
-		$this->assertInstanceOf( GaugeMetric::class, $metric );
-		$this->assertSame( 1, $metric->getSampleCount() );
-		$this->assertSame( floatval( $metricValue ), $samples[0]->getValue() );
-		$this->assertArrayEquals( $metricLabels, $samples[0]->getLabelValues(), true );
+		$this->assertSame(
+			$expectedStats,
+			$statsHelper->consumeAllFormatted()
+		);
 	}
 
 	public static function provideExecuteForMockIMetric() {
 		return [
-			'Not in verbose mode with labels' => [
-				true, [ 'wiki' => 'test' ], 1234, "mock_metric_name with label(s) test is 1234.\n",
+			'In verbose mode with labels' => [
+				true, [ 'wiki' => 'test' ], 1234,
+				"mock_metric_name with label(s) test is 1234.\n",
+				[ 'mediawiki.WikimediaEvents.mock_metric_name:1234|g|#wiki:test' ]
 			],
-			'In verbose mode with labels' => [ false, [ 'wiki' => 'test' ], 12345, "" ],
-			'Not in verbose mode with no labels' => [ true, [], 123, "mock_metric_name is 123.\n" ],
-			'In verbose mode with no labels' => [ false, [], 123567, "" ],
+			'Not in verbose mode with labels' => [ false, [ 'wiki' => 'test' ], 12345,
+				"",
+				[ 'mediawiki.WikimediaEvents.mock_metric_name:12345|g|#wiki:test' ]
+			],
+			'In verbose mode with no labels' => [ true, [], 123,
+				"mock_metric_name is 123.\n",
+				[ 'mediawiki.WikimediaEvents.mock_metric_name:123|g' ]
+			],
+			'Not in verbose mode with no labels' => [ false, [], 123567,
+				"",
+				[ 'mediawiki.WikimediaEvents.mock_metric_name:123567|g' ]
+			],
 		];
 	}
 }
