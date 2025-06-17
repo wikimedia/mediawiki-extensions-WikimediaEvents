dcfd6209340ca525c8daf09d2c70e7a594e64cf5
Sam Smith <phuedx@wikimedia.org>
2025-06-17 15:35:13 +0100

    ext.wikimediaEvents: Repurpose PageVisit instrument
    ... for the second synthetic A/A experiment.
    
    The Experiment Platform team plan to run a second synthetic A/A
    experiment on 1% of traffic to enwiki for a week in order to establish a
    baseline number of unique visitors for such a sample.
    
    Bug: T397138
    Change-Id: I3dc585a6d80f45f271f2e942f2e69554f8ae4b54

--- modules/ext.wikimediaEvents/xLab/pageVisit.js
+++ modules/ext.wikimediaEvents/xLab/pageVisit.js
@@ -1,12 +1,23 @@
 /**
  * A simple experiment-specific instrument that sends a "page-visited" event if the current user is
- * enrolled in the "sds2-4-11-synth-aa-test" experiment.
+ * enrolled in the "sds2-4-11-synth-aa-test-2" experiment.
  *
- * See https://phabricator.wikimedia.org/T393918 and its parent task
- * https://phabricator.wikimedia.org/T392313 for more context.
+ * History
+ * =======
+ *
+ * 2025-06-27:
+ *
+ * * This instrument was repurposed for the "sds2-4-11-synth-aa-test-2" experiment. See
+ *   https://phabricator.wikimedia.org/T397138 for more context.
+ *
+ * Previously:
+ *
+ * * This instrument was used for the "sds2-4-11-synth-aa-test-2" experiment. See
+ *   https://phabricator.wikimedia.org/T393918 and its parent task
+ *   https://phabricator.wikimedia.org/T392313 for more context.
  */
 
-const EXPERIMENT_NAME = 'sds2-4-11-synth-aa-test';
+const EXPERIMENT_NAME = 'sds2-4-11-synth-aa-test-2';
 
 mw.loader.using( 'ext.xLab' ).then( () => {
 	mw.xLab.getExperiment( EXPERIMENT_NAME )
