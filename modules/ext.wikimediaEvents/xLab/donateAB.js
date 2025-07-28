c61dd43d93741d457c186b911dd3dd5ee162423b
Clare Ming <cming@wikimedia.org>
2025-07-28 12:02:41 -0600

    Fix the conditional for the donate a/b test
    The isEnrolled() function is not a prototype method
    for the Experiment constructor
    
    Bug: T395944
    Bug: T396682
    Change-Id: I319b3fff785357699c888b2804567fbfe1699b83

--- modules/ext.wikimediaEvents/xLab/donateAB.js
+++ modules/ext.wikimediaEvents/xLab/donateAB.js
@@ -3,7 +3,7 @@ const { ClickThroughRateInstrument } = require( 'ext.wikimediaEvents.xLab' );
 mw.loader.using( 'ext.xLab' ).then( () => {
 	const experiment = mw.xLab.getExperiment( 'we-3-2-3-donate-ab-test-1' );
 
-	if ( experiment.isEnrolled() ) {
+	if ( experiment.isAssignedGroup( 'control', 'treatment' ) ) {
 		ClickThroughRateInstrument.start(
 			'.re-experiment-vector-donate-entry-point-variation',
 			'Donate Link',
