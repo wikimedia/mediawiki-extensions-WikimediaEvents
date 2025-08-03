4c4eb7c2ab86a32a52d1e61a93f829270dc7a1d6
Gergő Tisza <tgr.huwiki@gmail.com>
2025-08-03 14:48:41 +0200

    Add higher traffic rate limiting class for bots
    For now, only check the global bot flag. For local bot
    flags (if they are even desirable), we might need to solve T410878
    first.
    
    Ignore owner-only OAuth access tokens as we don't want high rate
    limits on tokens with a long expiry.
    
    Bug: T399632
    Depends-On: I9240cac29f0209778d29c5299bf80f63ee4ccbb7
    Depends-On: I1802b6975392f333d668fecec5a2afb124e4e44c
    Change-Id: Ib6622d4ee80e9d95ce0d32ab7f757b06e8a1a1d2

--- includes/WikimediaEventsHooks.php
+++ includes/WikimediaEventsHooks.php
@@ -12,9 +12,11 @@ use MediaWiki\Config\Config;
 use MediaWiki\Context\IContextSource;
 use MediaWiki\Context\RequestContext;
 use MediaWiki\Deferred\DeferredUpdates;
+use MediaWiki\Extension\CentralAuth\User\CentralAuthUser;
 use MediaWiki\Extension\ConfirmEdit\CaptchaTriggers;
 use MediaWiki\Extension\ConfirmEdit\Hooks;
 use MediaWiki\Hook\BeforeInitializeHook;
+use MediaWiki\Hook\GetSessionJwtDataHook;
 use MediaWiki\Hook\RecentChange_saveHook;
 use MediaWiki\Hook\SpecialSearchGoResultHook;
 use MediaWiki\Hook\SpecialSearchResultsHook;
@@ -65,8 +67,8 @@ class WikimediaEventsHooks implements
 	RecentChange_saveHook,
 	ResourceLoaderRegisterModulesHook,
 	MakeGlobalVariablesScriptHook,
-	AuthManagerLoginAuthenticateAuditHook
-
+	AuthManagerLoginAuthenticateAuditHook,
+	GetSessionJwtDataHook
 {
 	private AccountCreationLogger $accountCreationLogger;
 	private Config $config;
@@ -616,6 +618,19 @@ class WikimediaEventsHooks implements
 	private function maybeAddWatchlistTracking( OutputPage $out ): void {
 		if ( $out->getTitle() && $out->getTitle()->isSpecial( "Watchlist" ) ) {
 			$out->addModules( 'ext.wikimediaEvents.WatchlistBaseline' );
+		}
+	}
+
+	/** @inheritDoc */
+	public function onGetSessionJwtData( ?UserIdentity $user, array &$jwtData ): void {
+		if ( $user
+			&& !( $jwtData['ownerOnly'] ?? false )
+			&& ExtensionRegistry::getInstance()->isLoaded( 'CentralAuth' )
+		) {
+			$centralUser = CentralAuthUser::getInstance( $user );
+			if ( in_array( 'global-bot', $centralUser->getGlobalGroups() ) ) {
+				$jwtData['rlc'] = 'global-bot';
+			}
 		}
 	}
 }
