6adc2256e6263b0f96271572821f3f3a813fb6ab
Sam Smith <phuedx@wikimedia.org>
2025-10-01 11:21:38 +0100

    ext.wikimediaEvents: Tidy up xLab/readingListAB.js
    * Replace loadExperiment() with the experimentPromise module-level
      constant, which resolves with an Experiment instance for the
      "we-3-3-4-reading-list-test1" experiment
    
    * Don't check Experiment#assignedGroup before calling Experiment#send().
      Experiment#send() checks the user has been assigned a group before
      sending experiment-related analytics events
    
    Change-Id: I5d11e44dabf1d1a9a91b6585d08c97a0a76f5e3f

--- modules/ext.wikimediaEvents/xLab/readingListAB.js
+++ modules/ext.wikimediaEvents/xLab/readingListAB.js
@@ -2,63 +2,38 @@ const EXPERIMENT_NAME = 'we-3-3-4-reading-list-test1';
 const ACTION_SAVE = 'save_article_to_reading_list';
 const ACTION_REMOVE = 'remove_article_from_reading_list';
 
-let experiment = null;
-let experimentLoaded = false;
-
-/**
- * Load the experiment once and cache it
- *
- * @return {Promise<Object|null>} The experiment object or null
- */
-function loadExperiment() {
-	if ( experimentLoaded ) {
-		return Promise.resolve( experiment );
-	}
-
-	return mw.loader.using( 'ext.xLab' )
-		.then( () => {
-			try {
-				experiment = mw.xLab.getExperiment( EXPERIMENT_NAME );
-				experimentLoaded = true;
-				return experiment;
-			} catch ( error ) {
-				mw.log( 'Error loading experiment:', error );
-				return null;
-			}
-		} )
-		.catch( ( error ) => {
-			mw.log( 'Error loading xLab module:', error );
-			return null;
-		} );
-}
+/** @type {Promise<Experiment>} */
+const experimentPromise = mw.loader.using( 'ext.xLab' )
+	.then( () => mw.xLab.getExperiment( EXPERIMENT_NAME ) )
+	.catch( ( error ) => {
+		mw.log( 'Error loading ext.xLab module:', error );
+	} );
 
 /**
  * Track page visit event for the experiment
  */
 function trackPageVisit() {
-	loadExperiment().then( ( exp ) => {
-		if ( exp && exp.assignedGroup ) {
-			// Determine if referrer is internal or external
-			let actionSource = 'external_referer';
-			if ( document.referrer ) {
-				const referrerUrl = new URL( document.referrer );
-				const currentUrl = new URL( document.location.href );
-				actionSource = referrerUrl.hostname === currentUrl.hostname ? 'internal_referer' : 'external_referer';
-			}
-
-			exp.send( 'page_load', {
-				action_source: actionSource,
-				action_context: document.location.pathname
-			} );
-
-			// Track clicks on reading list bookmark icon
-			$( document ).find( '.mw-ui-icon-bookmarkList' ).on( 'click', () => {
-				exp.send( 'click', {
-					action_subtype: 'view_reading_list',
-					action_source: 'top_right'
-				} );
-			} );
+	experimentPromise.then( ( exp ) => {
+		// Determine if referrer is internal or external
+		let actionSource = 'external_referer';
+		if ( document.referrer ) {
+			const referrerUrl = new URL( document.referrer );
+			const currentUrl = new URL( document.location.href );
+			actionSource = referrerUrl.hostname === currentUrl.hostname ? 'internal_referer' : 'external_referer';
 		}
+
+		exp.send( 'page_load', {
+			action_source: actionSource,
+			action_context: document.location.pathname
+		} );
+
+		// Track clicks on reading list bookmark icon
+		$( document ).find( '.mw-ui-icon-bookmarkList' ).on( 'click', () => {
+			exp.send( 'click', {
+				action_subtype: 'view_reading_list',
+				action_source: 'top_right'
+			} );
+		} );
 	} );
 }
 
@@ -66,7 +41,7 @@ function trackPageVisit() {
  * Track clicks on reading list link in notifications
  */
 function trackNotificationClicks() {
-	loadExperiment().then( ( exp ) => {
+	experimentPromise.then( ( exp ) => {
 		if ( exp && exp.isAssignedGroup( 'treatment' ) ) {
 			document.addEventListener( 'click', ( event ) => {
 				const $link = $( event.target ).closest( 'a' );
@@ -96,14 +71,12 @@ function trackNotificationClicks() {
  * @param {string} source
  */
 function recordExperimentEvent( size, subtype, source ) {
-	loadExperiment().then( ( exp ) => {
-		if ( exp && exp.assignedGroup ) {
-			exp.send( 'click', {
-				action_subtype: subtype,
-				action_source: source,
-				action_context: JSON.stringify( { article_count: size } )
-			} );
-		}
+	experimentPromise.then( ( exp ) => {
+		exp.send( 'click', {
+			action_subtype: subtype,
+			action_source: source,
+			action_context: JSON.stringify( { article_count: size } )
+		} );
 	} );
 }
 
