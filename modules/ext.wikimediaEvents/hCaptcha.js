b71275a3dc0c9936cde28a1e9b3e9ac62a715f27
Kosta Harlan <kharlan@wikimedia.org>
2025-11-14 16:33:15 +0100

    hCaptcha: Record hcaptcha.execute() calls in VisualEditorFeatureUse
    Why:
    
    - We want to be able to more easily understand editing funnels for users
      who have hcaptcha.execute() run as part of submitting an edit form
    
    What:
    
    - Subscribe to the hcaptcha.execute() event emitted in ConfirmEdit's
      secureEnclave.js file, and emit an event to VisualEditorFeatureUse
    
    Bug: T410146
    Change-Id: I77cbd3c936d7f06d3ea679c66bdda6b3c274361f

--- modules/ext.wikimediaEvents/hCaptcha.js
+++ modules/ext.wikimediaEvents/hCaptcha.js
@@ -2,16 +2,15 @@
  * Frontend instrumentation for hCaptcha
  */
 function setupInstrumentation() {
+	// This dictionary maps the interfaceName values that are for editing interfaces
+	// to the string that should be used as editor_interface in the 'editAttemptStep'
+	// mw.track call. visualEditorFeatureUse handles editor_interface for us.
+	const editingInterfaces = { edit: 'wikitext', visualeditor: 'visualeditor' };
+
 	// Emit an event when various callbacks fire from hcaptcha.render()
 	mw.trackSubscribe( 'confirmEdit.hCaptchaRenderCallback', ( _, event, interfaceName ) => {
 		// ./specialCreateAccount/instrumentation.js already handles this
 		// when the interface is account creation, so only need to handle editing interfaces here
-
-		// This dictionary maps the interfaceName values that are for editing interfaces
-		// to the string that should be used as editor_interface in the 'editAttemptStep'
-		// mw.track call. visualEditorFeatureUse handles editor_interface for us.
-		const editingInterfaces = { edit: 'wikitext', visualeditor: 'visualeditor' };
-
 		if ( Object.keys( editingInterfaces ).includes( interfaceName ) ) {
 			if ( event === 'open' ) {
 				mw.track( 'editAttemptStep', {
@@ -31,6 +30,16 @@ function setupInstrumentation() {
 				// - expired
 				// - error
 				action: event
+			} );
+		}
+	} );
+
+	mw.trackSubscribe( 'stats.mediawiki_confirmedit_hcaptcha_execute_total', ( _, count, data ) => {
+		if ( Object.keys( editingInterfaces ).includes( data.interfaceName ) ) {
+			mw.track( 'visualEditorFeatureUse', {
+				feature: 'hcaptcha',
+				action: 'execute',
+				editor_interface: editingInterfaces[ data.interfaceName ]
 			} );
 		}
 	} );
