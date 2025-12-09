2a7882fcd66ad08c415e8644c10cb986e7406ff2
ksarabia <ksarabia@wikimedia.org>
2025-12-09 14:22:25 -0600

    Backport: Instrument sticky header session length to 1.46.0-wmf.5
    This change squashes and backports:
    - Gerrit 1213523
    - Gerrit 1212556
    - Gerrit 1214630
    
    Bug: T412146
    Change-Id: I441985bcedcaa7a111f5514861f0e9a08b81c790

--- OWNERS.md
+++ OWNERS.md
@@ -209,10 +209,10 @@ More details at <https://phabricator.wikimedia.org/T398495>
 Initial ticket: <https://phabricator.wikimedia.org/T399227>
 Round 2 ticket: <https://phabricator.wikimedia.org/T409190>
 
-## Reader Growth StickyHeaders session length instrumentation
+## Reader Growth StickyHeaders instrumentation
 
 * Since: December 2025
-* Files: xLab/stickyHeadersSessionLength.js
+* Files: xLab/stickyHeaders.js
 * Contact: Reader Growth
 
 More details at <https://phabricator.wikimedia.org/T411357>
--- modules/ext.wikimediaEvents/sessionLength/mixin.js
+++ modules/ext.wikimediaEvents/sessionLength/mixin.js
@@ -1,25 +1,33 @@
 'use strict';
 
 /**
- * NOTE: this is for experimental purposes only, for
- * the Web Team's Search Recommendations A/B test
- *
  * SessionLengthInstrumentMixin: Tracks session length.
- * Starts and stops ticks based on user activity, with a
- * reset after inactivity. Integrates with mw.eventLog for
- * analytics. Use `start(streamName, schemaID)` to begin
- * tracking and `stop(streamName)` to end tracking. Based
- * on sessionTick.js for modular reuse across core.
+ *
+ * Starts and stops ticks based on user activity, with a reset after inactivity.
+ * Integrates with `mw.eventLog` and `mw.xLab` for analytics.
+ *
+ * Use `start( streamName, schemaID )` or `start( experiment )` to begin tracking,
+ * Optional parameters:
+ * - interaction data, defaults to empty object
+ * - session tick in milliseconds, defaults to {@link TICK_MS}
+ * - session reset in milliseconds, defaults to {@link RESET_MS}
+ *
+ * Use `stop( streamName | experiment )` to end tracking.
+ *
+ * Based on `sessionTick.js` for modular reuse across core and extensions.
  */
 
-// Constants for session timing, idle, reset, and tick limits.
 const NOOP = function () { };
 
-const TICK_MS = 30000;
-const IDLE_MS = 100000;
-const RESET_MS = 1800000;
-const DEBOUNCE_MS = 5000;
-const TICK_LIMIT = Math.ceil( RESET_MS / TICK_MS );
+// Defaults for session tick, idle, reset, and debounce.
+// All values in milliseconds.
+const TICK_MS = 30 * 1000;
+const IDLE_MS = 100 * 1000;
+// Reset the session after 1 hour.
+// Value based on previous analysis at
+// https://people.wikimedia.org/~jiawang/web/empty_recommendation_analysis_report.html#session-length.
+const RESET_MS = 60 * 60 * 1000;
+const DEBOUNCE_MS = 5 * 1000;
 
 const KEY_LAST_TIME = 'mp-sessionTickLastTickTime';
 const KEY_COUNT = 'mp-sessionTickTickCount';
@@ -64,8 +72,8 @@ function sessionReset() {
 	mw.storage.session.set( KEY_COUNT, 0 );
 }
 
-function sessionTick( incr ) {
-	if ( incr > TICK_LIMIT ) {
+function sessionTick( incr, tickLimit ) {
+	if ( incr > tickLimit ) {
 		throw new Error( 'Session ticks exceed limit' );
 	}
 
@@ -100,7 +108,8 @@ function sessionTick( incr ) {
 }
 
 // Main regulator function to manage session ticking.
-function regulator() {
+function regulator( reset, tick ) {
+	const tickLimit = Math.ceil( reset / tick );
 	let tickTimeout = null;
 	let idleTimeout = null;
 	let debounceTimeout = null;
@@ -110,17 +119,17 @@ function regulator() {
 		const now = Date.now();
 		const gap = now - ( Number( mw.storage.session.get( KEY_LAST_TIME ) ) || 0 );
 		const count = Number( mw.storage.session.get( KEY_COUNT ) ) || 0;
-		if ( count === 0 || gap > RESET_MS ) {
+		if ( count === 0 || gap > reset ) {
 			// Reset session if idle time exceeds limit.
 			mw.storage.session.set( KEY_LAST_TIME, now );
 			sessionReset();
-			sessionTick( 1 ); // Start tick
-		} else if ( gap > TICK_MS ) {
+			sessionTick( 1, tickLimit ); // Start tick
+		} else if ( gap > tick ) {
 			mw.storage.session.set( KEY_LAST_TIME, now );
-			sessionTick( 1 );
+			sessionTick( 1, tickLimit );
 		}
 		// Schedule next tick.
-		tickTimeout = setTimeout( run, TICK_MS );
+		tickTimeout = setTimeout( run, tick );
 	}
 
 	// Stops all timeouts to mark session as inactive.
@@ -180,15 +189,17 @@ function regulator() {
 const SessionLengthInstrumentMixin = {
 	state,
 	/**
-	 * start( streamName, schemaID, ?data ) - global submitInteraction()
-	 * start( instrument, ?data ) - calls instrument.submitInteraction()
-	 * start( experiment, ?data ) - calls experiment.submitInteraction()
+	 * start( streamName, schemaID ) - global submitInteraction()
+	 * start( instrument ) - calls instrument.submitInteraction()
+	 * start( experiment ) - calls experiment.submitInteraction()
 	 *
-	 * @param {string|Instrument|mw.xLab.Experiment} [dest] string, Instrument or Experiment
-	 * @param {string|Object} [schemaOrData] string or data object
-	 * @param {Object?} [data]
+	 * @param {string|Instrument|Experiment} [dest] string, Instrument or Experiment
+	 * @param {string|Object} [schemaOrData={}] string or data object
+	 * @param {Object} [data={}] interaction data
+	 * @param {number} [reset=RESET_MS] session reset in milliseconds
+	 * @param {number} [tick=TICK_MS] session tick in milliseconds
 	 */
-	start( dest, schemaOrData = {}, data = {} ) {
+	start( dest, schemaOrData = {}, data = {}, reset = RESET_MS, tick = TICK_MS ) {
 		if ( typeof dest === 'string' ) {
 			const streamName = dest;
 			const schemaID = schemaOrData;
@@ -208,10 +219,10 @@ const SessionLengthInstrumentMixin = {
 		}
 
 		// Start algorithm
-		regulator();
+		regulator( reset, tick );
 	},
 	/**
-	 * @param {string|Instrument|mw.xLab.Experiment} [dest] string, Instrument, or Experiment
+	 * @param {string|Instrument|Experiment} [dest] string, Instrument, or Experiment
 	 */
 	stop( dest ) {
 		state.delete( dest );
