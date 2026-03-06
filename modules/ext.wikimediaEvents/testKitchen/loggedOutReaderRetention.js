989e53103521562db334924ed8742c84afdbd043
Sam Smith <phuedx@wikimedia.org>
2026-03-06 11:46:19 +0000

    ext.wikimediaEvents: pageVisit -> loggedOutReaderRetention
    What?
    
    Repurpose the code in pageVisit.js to measure logged-out reader
    retention on a recurring basis.
    
    Bug: T419191
    Depends-On: I0358ab7f9300d4d40218c90a232a1848b1877a70
    Change-Id: I8a590c84ce1b0ee0769fc1dd340733e87d4dc2c9

--- modules/ext.wikimediaEvents/testKitchen/loggedOutReaderRetention.js
+++ modules/ext.wikimediaEvents/testKitchen/loggedOutReaderRetention.js
@@ -1,9 +1,17 @@
 /**
- * A simple experiment-specific instrument that sends a "page-visited" event if the current user is
- * enrolled in the "sds2-4-11-synth-aa-test-2" experiment.
+ * Used to measure logged-out retention on a monthly basis.
+ *
+ * See:
+ *
+ * * [T409190 Start new A/A test for retenion](https://phabricator.wikimedia.org/T409190)
+ * * [T419191 Start new A/A test for reader retention for March 2026](https://phabricator.wikimedia.org/T419191)
  *
  * History
  * =======
+ *
+ * 2026-03-06:
+ * The instrument was repurposed to measure logged-out reader retention on a monthly basis. It was
+ * renamed from `pageVisit.js` to `loggedOutReaderRetention.js` to reflect this.
  *
  * 2026-02-05:
  * This instrument was resurrected and repurposed for the "synth-aaa-test-mw-js" experiment. This
@@ -23,14 +31,15 @@
  * https://phabricator.wikimedia.org/T392313 for more context.
  */
 
-const EXPERIMENT_NAME = 'synth-aaa-test-mw-js';
+// e.g. logged-out-retention-round1, logged-out-retention-round2, etc.
+const EXPERIMENT_NAME_PREFIX = 'logged-out-retention-';
 
 mw.loader.using( 'ext.testKitchen' ).then( () => {
-	mw.testKitchen.getExperiment( EXPERIMENT_NAME )
+	mw.testKitchen.getExperimentByPrefix( EXPERIMENT_NAME_PREFIX )
 		.send(
 			'page-visited',
 			{
-				instrument_name: 'PageVisit'
+				instrument_name: 'LoggedOutReaderRetention'
 			}
 		);
 } );
