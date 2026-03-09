56dbd2d699d6a777b1fdc7afa2e53ed115e68de9
Santiago Faci <sfaci@wikimedia.org>
2026-03-09 10:31:07 +0100

    ext.wikimediaEvents: Updated Test Kitchen impact test experiment
    We agreed on increasing the aggregated traffic to 10% for this experiment and also
    doing it with 3 similar experiments instead of only one, to also check
    that groups are assigned equally.
    
    This patch updates the Test Kitchen test impact experiment by creating
    three similar experiments.
    
    Bug: T407570
    Change-Id: Ic0718309461aed1c0a6b36df384f541ad7067921
    (cherry picked from commit 9cc9012d9a635de4bebe8b472fdacf36f06f5554)

--- modules/ext.wikimediaEvents/testKitchen/impactTest.js
+++ modules/ext.wikimediaEvents/testKitchen/impactTest.js
@@ -1,18 +1,23 @@
 /**
  * Experiment-specific instrument that sends a `page-visited` event if the user is enrolled in the experiment below
  * The purpose of the experiment is to measure the impact of incremental increase in traffic for cache splitting experiments
+ *
  * Once the impact test finished, this experiment will be deleted
  * See https://phabricator.wikimedia.org/T407570 for more details
  */
 
-const EXPERIMENT_NAME = 'synth-aa-test-traffic-impact';
+const EXPERIMENT_NAME = 'synth-aa-test-traffic-impact-';
+const EXPERIMENT_NAME_1 = EXPERIMENT_NAME + '1';
+const EXPERIMENT_NAME_2 = EXPERIMENT_NAME + '2';
+const EXPERIMENT_NAME_3 = EXPERIMENT_NAME + '3';
 const INSTRUMENT_NAME = 'PageVisit';
 
 mw.loader.using( 'ext.testKitchen' ).then( () => {
-	const experiment = mw.testKitchen.getExperiment( EXPERIMENT_NAME );
-
-	experiment.send(
-		'page-visited',
-		{ instrument_name: INSTRUMENT_NAME }
-	);
+	[ EXPERIMENT_NAME_1, EXPERIMENT_NAME_2, EXPERIMENT_NAME_3 ].forEach( ( experimentName ) => {
+		mw.testKitchen.getExperiment( experimentName )
+			.send(
+				'page-visited',
+				{ instrument_name: INSTRUMENT_NAME }
+			);
+	} );
 } );
