5bfd3f9f66eb5ebd0b0bc2a146324a360ddd009c
Michael Große <mgrosse@wikimedia.org>
2026-03-16 12:48:58 +0100

    createAccount: Log exposure and CTRs for account creation experiment
    These will be used in the we-1-8-account-creation-form-v1 experiment.
    
    Bug: T419916
    Change-Id: Ia922b58e0750120c42ef6d34e0d435e720e6f8e4
    Depends-On: I68287d3e21bfe9dc35adfdd200e086fa2996e52b
    (cherry picked from commit dc936b403f20c178d18ba3421e42b9702fde0656)

--- modules/ext.wikimediaEvents/specialCreateAccount/init.js
+++ modules/ext.wikimediaEvents/specialCreateAccount/init.js
@@ -1,6 +1,8 @@
 const setupInstrumentation = require( './instrumentation.js' );
+const setupWe18V1ExperimentInstrumentation = require( './experimentInstrumentation.js' );
 
 // Don't run instrumentation automatically in QUnit tests.
 if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'CreateAccount' ) {
 	$( setupInstrumentation );
+	$( setupWe18V1ExperimentInstrumentation );
 }
