bb83d8fe1ec1902278f7ff3d9b3651802fd70a95
Santiago Faci <sfaci@wikimedia.org>
2026-03-18 12:56:24 +0100

    ext.wikimediaEvents.mobileSectionSwitch: Updated web/base schema version
    The last version of `web/base` schema and Test Kitchen platform introduced a breaking change recently because it sopped accepting `xLab` as a valid `coordinator` value (now `default` is the right one for similar cases).
    
    Test Kitchen started to use that new version, `2.0.0` by default. Any
    explicit usage of that schema version will cause schema validation
    errors because of invalid `coordinator` values.
    
    This change removes the explicit usage of `web/base` 1.5.0 as the schema
    for this experiment. That way, 2.0.0 will be the used version.
    
    Change-Id: I8c0569a307d3396e6e0115653f8c18a0540044a8

--- modules/ext.wikimediaEvents/mobileSectionSwitch.js
+++ modules/ext.wikimediaEvents/mobileSectionSwitch.js
@@ -1,13 +1,11 @@
 const editingSessionService = require( './editingSessionService.js' );
 
 const EXPERIMENT_NAME = 'fy25-26-we-1-1-19-mobile-section-dead-end-phase-2';
-const SCHEMA_NAME = '/analytics/product_metrics/web/base/1.5.0';
 const STREAM_NAME = 'mediawiki.product_metrics.contributors.experiments';
 
 const experimentPromise = mw.loader.using( 'ext.testKitchen' )
 	.then( () => {
 		const experiment = mw.testKitchen.getExperiment( EXPERIMENT_NAME );
-		experiment.setSchema( SCHEMA_NAME );
 		experiment.setStream( STREAM_NAME );
 		return experiment;
 	} )
