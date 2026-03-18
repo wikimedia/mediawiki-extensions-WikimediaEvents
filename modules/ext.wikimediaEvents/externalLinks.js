b7d6e08a4e82d98c1e8ad5d4a298c0a26957f9b0
Marcin Szwarc <mszwarc@wikimedia.org>
2026-03-18 12:25:05 +0100

    Normalize external domain names in click analysis
    Why:
    - We'd like to clean up our data from clusters of subdomains like
      a.example.com, b.example.com, c.example.com, when we're interested
      only in example.com in general.
    
    What:
    - Send the domain name, as specified in the config (but still match on
      subdomains).
    
    Bug: T419837
    Change-Id: I16c1bdc8980f542eaf128341b1049179de4165ee

--- modules/ext.wikimediaEvents/externalLinks.js
+++ modules/ext.wikimediaEvents/externalLinks.js
@@ -6,14 +6,14 @@
 
 const config = require( './config.json' );
 
-function shouldTrackLink( host ) {
+function getTrackingPrefix( host ) {
 	host = '.' + host;
 	for ( const prefix of config.WikimediaEventsExternalLinkTrackedDomains ) {
 		if ( host.endsWith( '.' + prefix ) ) {
-			return true;
+			return prefix;
 		}
 	}
-	return false;
+	return null;
 }
 
 function setupInstrumentation() {
@@ -29,20 +29,23 @@ function setupInstrumentation() {
 	// eslint-disable-next-line no-jquery/no-global-selector
 	const $links = $( '.mw-parser-output a.external' );
 	$links.on( 'mousedown', function () {
-		// Ensure that this host is of interest to us and that it's a valid value for Prometheus
-		if (
-			!this.host ||
-			!shouldTrackLink( this.host ) ||
-			!/^[A-Za-z0-9_.+-]+$/.test( this.host ) ) {
+		if ( !this.host ) {
+			return;
+		}
+
+		// Ensure that this host is of interest to us and normalize it to just the domain given in
+		// config (to aggregate over subdomains)
+		const trackingPrefix = getTrackingPrefix( this.host );
+		if ( trackingPrefix === null ) {
 			return;
 		}
 
 		// Prometheus doesn't support labels with dots in values
-		const host = this.host.replace( /\./g, '_' );
+		const domain = trackingPrefix.replace( /\./g, '_' );
 		const wiki = mw.config.get( 'wgDBname' );
 		// Normally, we'd use mw.track here, but events created this way don't get sent in Chrome before
 		// leaving the page (T419956). This is critical, as most link clicks lead to the user leaving the page.
-		const serializedData = `mediawiki_WikimediaEvents_extLinkClick_total:1|c%7C%23wiki:${ wiki },domain:` + host;
+		const serializedData = `mediawiki_WikimediaEvents_extLinkClick_total:1|c%7C%23wiki:${ wiki },domain:` + domain;
 		mw.eventLog.sendBeacon( config.WMEStatsBeaconUri + '?' + serializedData );
 	} );
 }
