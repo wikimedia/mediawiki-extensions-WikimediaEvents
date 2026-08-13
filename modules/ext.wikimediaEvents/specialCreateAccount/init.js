93fa72245f81b1967b3063b23c0f4ba62c3cc96f
Michael Große <mgrosse@wikimedia.org>
2026-08-13 18:57:27 +0200

    refactor: clean up the old account creation experiments
    This change works alongside Id44c68fbd3e5f46b3bcd0e0e5c87b057ca6a8263.
    
    Additional cleanup work is happening in I4ea43f6c9de334caaf0e774f3e16fdb2f03b78bf.
    
    Bug: T433783
    Change-Id: Ia8e13c7156ba73e696aafe2af0b04ec188e0bd54

--- modules/ext.wikimediaEvents/specialCreateAccount/init.js
+++ modules/ext.wikimediaEvents/specialCreateAccount/init.js
@@ -1,9 +1,4 @@
 const setupInstrumentation = require( './instrumentation.js' );
-const setupWe18NoDesktopBenefitsExperimentInstrumentation = require( './experimentInstrumentation.js' );
-const decorateCreateAccountLinks = require( './experimentDecorateCreateAccountLinks.js' );
-
-// On all pageviews add an URL param to all links to Special:CreateAccount and Special:UserLogin
-decorateCreateAccountLinks();
 
 // Don't run instrumentation automatically in QUnit tests.
 if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'CreateAccount' ) {
@@ -11,6 +6,5 @@ if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'CreateAccount' ) {
 		'ext.wikimediaEvents.testKitchen'
 	).then( () => {
 		$( setupInstrumentation );
-		$( setupWe18NoDesktopBenefitsExperimentInstrumentation );
 	} );
 }
