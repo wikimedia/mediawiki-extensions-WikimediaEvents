4d2b0bcd734a4a2d785f42e16adeb73d11188703
Michael Große <mgrosse@wikimedia.org>
2026-08-26 13:14:45 +0200

    instrument: record edit_saved events for early onboarding experiment
    These are needed for the permanent account creation rate and probably
    also for the editor retention rates.
    
    Bug: T432809
    Change-Id: I92a5067b5e5b3493ad17ff2a46b722c0649c33e4

--- modules/ext.wikimediaEvents/earlyOnboarding.js
+++ modules/ext.wikimediaEvents/earlyOnboarding.js
@@ -1,4 +1,8 @@
 const anyPageVisit = require( './anyPageVisit.js' );
+const editSaved = require( './editSaved.js' );
 mw.testKitchen.getExperiment( 'de-1-3-1-specialhomepage-onboarding-aa-test' ).then(
-	( e ) => e.use( anyPageVisit() )
+	( e ) => {
+		e.use( anyPageVisit() );
+		e.use( editSaved );
+	}
 );
