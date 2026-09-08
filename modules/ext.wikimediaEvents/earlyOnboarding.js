3001b7ceb00218e9854f1e2eaa64efcb658085ae
Michael Große <mgrosse@wikimedia.org>
2026-09-08 21:08:27 +0200

    instrument(AccountSetup): switch to recording for the A/B test
    These metrics are used for the reader and editor retention, and edit
    rates.
    
    Bug: T432164
    Change-Id: I4f00319341eec348f641633467971aeac6f6435b

--- modules/ext.wikimediaEvents/earlyOnboarding.js
+++ modules/ext.wikimediaEvents/earlyOnboarding.js
@@ -1,6 +1,6 @@
 const { anyPageVisit } = require( 'ext.wikimediaEvents.testKitchen' );
 const editSaved = require( './editSaved.js' );
-mw.testKitchen.getExperiment( 'de-1-3-1-specialhomepage-onboarding-aa-test' ).then(
+mw.testKitchen.getExperiment( 'de-1-3-1-specialhomepage-onboarding-ab-test' ).then(
 	( e ) => {
 		e.use( anyPageVisit() );
 		e.use( editSaved );
