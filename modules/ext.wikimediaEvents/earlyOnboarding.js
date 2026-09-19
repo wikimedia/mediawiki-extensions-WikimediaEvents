13ef2e350970c2e690f205d5cddcc1e6f586fd18
Michael Große <mgrosse@wikimedia.org>
2026-09-19 13:26:44 +0200

    EarlyOnboarding: collect referrer on page visit events
    Applying `true` to the `recordReferrerClass` option in anyPageVisit.
    
    Bug: T437339
    Change-Id: I3e6b0d88e2e52acfc122e43d4ba8b5e811de16b3

--- modules/ext.wikimediaEvents/earlyOnboarding.js
+++ modules/ext.wikimediaEvents/earlyOnboarding.js
@@ -2,7 +2,9 @@ const { anyPageVisit } = require( 'ext.wikimediaEvents.testKitchen' );
 const editSaved = require( './editSaved.js' );
 mw.testKitchen.getExperiment( 'de-1-3-1-specialhomepage-onboarding-ab-test' ).then(
 	( e ) => {
-		e.use( anyPageVisit() );
+		// Record the visit's referrer class in the event's `action_source`.
+		// Refer to 'anyPageVisit.js' T434837.
+		e.use( anyPageVisit( { recordReferrerClass: true } ) );
 		e.use( editSaved );
 	}
 );
